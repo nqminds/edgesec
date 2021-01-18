f775308e96236bd0c8d383a2ad5f7030d97eab97
Alexandru Mereacre <mereacre@gmail.com>
2021-01-18 18:30:11 +0000

    wip: network segmentation docs

--- src/edgesec.c
+++ src/edgesec.c
@@ -1,25 +1,27 @@
+/****************************************************************************
+ * Copyright (C) 2020 by NQMCyber Ltd                                       *
+ *                                                                          *
+ * This file is part of EDGESec.                                            *
+ *                                                                          *
+ *   EDGESec is free software: you can redistribute it and/or modify it     *
+ *   under the terms of the GNU Lesser General Public License as published  *
+ *   by the Free Software Foundation, either version 3 of the License, or   *
+ *   (at your option) any later version.                                    *
+ *                                                                          *
+ *   EDGESec is distributed in the hope that it will be useful,             *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *   GNU Lesser General Public License for more details.                    *
+ *                                                                          *
+ *   You should have received a copy of the GNU Lesser General Public       *
+ *   License along with EDGESec. If not, see <http://www.gnu.org/licenses/>.*
+ ****************************************************************************/
+
 /**
-
-@mainpage EDGESec
-@author Alexandru Mereacre
-
-Copyright (C) 2020 by NQMCyber Ltd
-
-This file is part of EDGESec
-
-EDGESec is free software: you can redistribute it and/or modify it     
-under the terms of the GNU Lesser General Public License as published  
-by the Free Software Foundation, either version 3 of the License, or   
-(at your option) any later version.
-
-EDGESec is distributed in the hope that it will be useful,             
-but WITHOUT ANY WARRANTY; without even the implied warranty of         
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          
-GNU Lesser General Public License for more details.                    
-
-You should have received a copy of the GNU Lesser General Public       
-License along with EDGESec. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * @file edgesec.c 
+ * @author Alexandru Mereacre 
+ * @brief File containing the edgesec tool implementations.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
