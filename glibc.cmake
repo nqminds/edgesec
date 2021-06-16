ae497cd130dd1e56e4df8490ca4974211f6a6094
Alexandru Mereacre <mereacre@gmail.com>
2021-06-16 13:27:29 +0100

    glibc version check

---
+++ glibc.cmake
@@ -0,0 +1,27 @@
+# - Check glibc version
+# CHECK_GLIBC_VERSION()
+# 
+# Once done this will define
+#
+#   GLIBC_VERSION - glibc version
+#
+
+MACRO (CHECK_GLIBC_VERSION)
+    EXECUTE_PROCESS (
+        COMMAND ${CMAKE_C_COMPILER} -print-file-name=libc.so.6
+	OUTPUT_VARIABLE GLIBC
+	OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+    GET_FILENAME_COMPONENT (GLIBC ${GLIBC} REALPATH)
+    GET_FILENAME_COMPONENT (GLIBC_VERSION ${GLIBC} NAME)
+    STRING (REPLACE "libc-" "" GLIBC_VERSION ${GLIBC_VERSION})
+    STRING (REPLACE ".so" "" GLIBC_VERSION ${GLIBC_VERSION})
+    IF (NOT GLIBC_VERSION MATCHES "^[0-9.]+$")
+        MESSAGE (FATAL_ERROR "Unknown glibc version: ${GLIBC_VERSION}")
+    ELSE ()
+        MESSAGE ("glibc version ${GLIBC_VERSION} found")
+        IF (GLIBC_VERSION STREQUAL "2.2.1")
+        MESSAGE (FATAL_ERROR "Unssuported version of glibc")
+        ENDIF ()    
+    ENDIF ()
+ENDMACRO (CHECK_GLIBC_VERSION)
