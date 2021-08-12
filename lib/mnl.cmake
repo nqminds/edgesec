4dbbc849a4de4b7f33a749d81282988ca8a41bc3
Alexandru Mereacre <mereacre@gmail.com>
2021-08-12 11:21:57 +0100

    Added no default path

--- lib/mnl.cmake
+++ lib/mnl.cmake
@@ -5,7 +5,7 @@ if (BUILD_MNL_LIB AND NOT (BUILD_ONLY_DOCS))
   set(LIBMNL_INCLUDE_DIR "${LIBMNL_INSTALL_DIR}/include")
   set(LIBMNL_LIB_DIR "${LIBMNL_INSTALL_DIR}/lib")
 
-  find_library(LIBMNL_LIB NAMES mnl libmnl PATHS "${LIBMNL_LIB_DIR}")
+  find_library(LIBMNL_LIB NAMES mnl libmnl PATHS "${LIBMNL_LIB_DIR}" NO_DEFAULT_PATH)
   if (LIBMNL_LIB)
     message("Found libmnl library: ${LIBMNL_LIB}")
   ELSE ()
@@ -29,6 +29,6 @@ if (BUILD_MNL_LIB AND NOT (BUILD_ONLY_DOCS))
       WORKING_DIRECTORY "${LIBMNL_SOURCE_DIR}"
     )
 
-    find_library(LIBMNL_LIB NAMES mnl libmnl PATHS "${LIBMNL_LIB_DIR}")
+    find_library(LIBMNL_LIB NAMES mnl libmnl PATHS "${LIBMNL_LIB_DIR}" NO_DEFAULT_PATH)
   endif ()
 endif ()
