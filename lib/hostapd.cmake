806a5ce31990b4f766c93d13259d0b638a4c41e4
Alois Klink <alois@nquiringminds.com>
2021-09-06 16:24:51 +0000

    build: Fix CMake compilation on RPi Ubuntu 20.04
    That was a bit difficult.
    
    It seems like the problem was that hostapd required OpenSSL
    to already be compiled.
    
    I have modified both OpenSSL and Hostapd.cmake files, so that
    they use ExternalProject_Add() and now build at build-time,
    so we can use multi-core compiling.

--- lib/hostapd.cmake
+++ lib/hostapd.cmake
@@ -3,34 +3,19 @@ if (BUILD_HOSTAPD AND NOT (BUILD_ONLY_DOCS))
   include(ExternalProject)
 
   ExternalProject_Add(hostapd_external
-    URL https://w1.fi/releases/hostapd-2.9.tar.gz
-    CONFIGURE_COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_LIST_DIR}/hostap/hostapd/.config <SOURCE_DIR><SOURCE_SUBDIR>/.config
+    # the URL must be one that has both hostapd AND wpa_supplicant in it
+    URL https://w1.fi/cgit/hostap/snapshot/hostap_2_9.tar.bz2
+    # call custom script to configure hostapd
+    CONFIGURE_COMMAND ${CMAKE_COMMAND}
+      -D INSTALL_CONFIG=<SOURCE_DIR><SOURCE_SUBDIR>/.config
+      -D CFLAGS="-I${LIBOPENSSL_INCLUDE_PATH}"
+      -D LIBS="-L${LIBOPENSSL_LIB_PATH}"
+      -D BINDIR="<INSTALL_DIR>/bin"
+      -P ${CMAKE_CURRENT_LIST_DIR}/hostapd-configure.cmake
     BUILD_IN_SOURCE true
     SOURCE_SUBDIR "./hostapd" # does nothing since we're not configuring with cmake
   )
-
-  if (FALSE)
-    set(HOSTAPD_SOURCE_DIR "${CMAKE_SOURCE_DIR}/lib/hostap/hostapd")
-    set(HOSTAPD_INSTALL_DIR "${CMAKE_CURRENT_BINARY_DIR}")
-    find_program(HOSTAPD NAMES hostapd PATHS "${HOSTAPD_INSTALL_DIR}" NO_DEFAULT_PATH)
-    if (HOSTAPD)
-      message("Found hostapd program: ${HOSTAPD}")
-    ELSE ()
-      message("Building hostapd...")
-      execute_process(
-        COMMAND make
-        WORKING_DIRECTORY "${HOSTAPD_SOURCE_DIR}"
-      )
-      execute_process(
-        COMMAND cp ./hostapd "${HOSTAPD_INSTALL_DIR}"
-        WORKING_DIRECTORY "${HOSTAPD_SOURCE_DIR}"
-      )
-      execute_process(
-        COMMAND make clean
-        WORKING_DIRECTORY "${HOSTAPD_SOURCE_DIR}"
-      )
-      find_program(HOSTAPD NAMES hostapd PATHS "${HOSTAPD_INSTALL_DIR}" NO_DEFAULT_PATH)
-    endif ()
-  endif()
+  ExternalProject_Get_Property(hostapd_external install_dir)
+  set(HOSTAPD_INSTALL_DIR "${install_dir}")
+  set(HOSTAPD "${HOSTAPD_INSTALL_DIR}/bin/hostapd")
 endif ()
-
--- lib/openssl.cmake
+++ lib/openssl.cmake
@@ -1,14 +1,17 @@
 if (BUILD_OPENSSL_LIB AND NOT (BUILD_ONLY_DOCS))
   add_compile_definitions(WITH_OPENSSL_SERVICE)
-  set(LIBOPENSSL_INSTALL_ROOT "${CMAKE_CURRENT_BINARY_DIR}/lib")
-  set(LIBOPENSSL_INSTALL_DIR "${LIBOPENSSL_INSTALL_ROOT}/openssl")
-  set(LIBOPENSSL_INCLUDE_PATH ${LIBOPENSSL_INSTALL_DIR}/include)
-  set(LIBOPENSSL_LIB_PATH ${LIBOPENSSL_INSTALL_DIR}/lib)
-  find_library(LIBCRYPTO_LIB NAMES libcrypto.a PATHS "${LIBOPENSSL_LIB_PATH}" NO_DEFAULT_PATH)
-  if (LIBCRYPTO_LIB)
-    message("Found libcrypto library: ${LIBCRYPTO_LIB}")
-  ELSE ()
-    execute_process(COMMAND bash ${CMAKE_SOURCE_DIR}/lib/compile_openssl.sh ${LIBOPENSSL_INSTALL_ROOT})
-    find_library(LIBCRYPTO_LIB NAMES libcrypto.a PATHS "${LIBOPENSSL_LIB_PATH}" NO_DEFAULT_PATH)
-  endif ()
+
+  include(ExternalProject)
+  ExternalProject_Add(openssl_external
+    URL https://github.com/openssl/openssl/archive/refs/tags/openssl-3.0.0-beta1.tar.gz
+    CONFIGURE_COMMAND <SOURCE_DIR>/Configure --prefix=<INSTALL_DIR> --openssldir=<INSTALL_DIR> -lpthread no-dtls no-dtls1 no-psk no-srp no-ec2m no-weak-ssl-ciphers no-dso no-engine no-threads
+    BUILD_IN_SOURCE true
+  )
+  ExternalProject_Get_Property(openssl_external install_dir)
+  set(LIBOPENSSL_INSTALL_DIR "${install_dir}")
+  set(LIBOPENSSL_INCLUDE_PATH "${LIBOPENSSL_INSTALL_DIR}/include")
+  set(LIBOPENSSL_LIB_PATH "${LIBOPENSSL_INSTALL_DIR}/lib")
+  add_library(LIBCRYPTO_LIB STATIC IMPORTED "${LIBOPENSSL_LIB_PATH}/libcrypto.a")
+
+  message("LIBCRYPTO will be built at ${LIBCRYPTO_LIB}")
 endif ()
