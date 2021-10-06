40b6d1bb3ac4ac5d81b0a43b716dfe007601c395
Alexandru Mereacre <mereacre@gmail.com>
2021-10-06 17:01:12 +0100

    wip: monitor app

--- apps/monitor/pages/_app.js
+++ apps/monitor/pages/_app.js
@@ -1,7 +1,15 @@
-import 'tailwindcss/tailwind.css'
+
+import { useRouter } from "next/router";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import "tailwindcss/tailwind.css";
+
+config.autoAddCss = false
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+  const {page} = router.query;
+  return <Component {...{page, ...pageProps}} />
 }
 
 export default MyApp
