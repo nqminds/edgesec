65a76b595999db28a8ff8315b28446ed7a52b923
Alois Klink <alois.klink@gmail.com>
2021-10-22 13:41:23 +0100

    Download libmnl source code

--- lib/mnl.cmake
+++ lib/mnl.cmake
@@ -10,10 +10,18 @@ if (BUILD_MNL_LIB AND NOT (BUILD_ONLY_DOCS))
   if (LIBMNL_LIB)
     message("Found libmnl library: ${LIBMNL_LIB}")
   ELSE ()
+    message("Install MNL vars not set, compiling our own version")
+    FetchContent_Declare(
+      libmnl_src
+      URL https://www.netfilter.org/pub/libmnl/libmnl-1.0.4.tar.bz2
+      URL_HASH SHA256=171f89699f286a5854b72b91d06e8f8e3683064c5901fb09d954a9ab6f551f81
+    )
+    FetchContent_Populate(libmnl_src)
+
     execute_process(COMMAND
       bash
       ${CMAKE_SOURCE_DIR}/lib/compile_mnl.sh
-      ${LIBMNL_SOURCE_DIR}
+      ${libmnl_src_SOURCE_DIR}
       ${LIBMNL_INSTALL_ROOT}
       ${target_autoconf_triple}
     )
