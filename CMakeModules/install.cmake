0aa674c056f556a188eeecb970b23c3314c5676a
Alois Klink <alois@nquiringminds.com>
2021-10-26 19:16:27 +0100

    Make CMAKE_INSTALL_PREFIX updatable
    Pretty painful to do suprisingly.
    
    Firstly, I made all variables that depend on CMAKE_INSTALL_PREFIX
    non-cache variables, so that they always get recalculated.
    
    This means if we change CMAKE_INSTALL_PREFIX,
    all subvals get updated as well.
    
    Next, since CMAKE_INSTALL_PREFIX can be changed after the build step,
    I added a relative path to RPATH.
    
    This means that edgesec will first search for the shared libraries
    in the originally configured libdir, but will also search for the
    shared libraries in ../lib, in case they were installed elsewhere
    (or you zip up the entire folder structure and move it somewhere else).
    
    Next, I made the following changes to config.ini:
    
      - configure_file() is now run during the install() step,
        not the configure step.
        This means that paths in config.ini will point to the correct place.
        This is done by the `CMakeModules/InstallConfigFile.cmake` script.
      - configure_file() now currently takes into account DESTDIR.

--- CMakeModules/install.cmake
+++ CMakeModules/install.cmake
@@ -25,22 +25,28 @@ endif ()
 # usually /usr/local/lib/edgesec (or /usr/lib/edgesec for .deb)
 # EDGESEC_private_lib_dir is set in main CMakeLists.txt, as we need it to set RPATH before targets
 # currently only hostapd, so it doesn't conflict with other hostapds
-set(EDGESEC_libexec_dir "${CMAKE_INSTALL_FULL_LIBEXECDIR}/${_project_lower}" CACHE PATH "Directory of private EDGESec bins")
-set(EDGESEC_config_dir "${CMAKE_INSTALL_FULL_SYSCONFDIR}/${_project_lower}" CACHE PATH "Directory of EDGESec config files")
-set(EDGESEC_log_dir "${CMAKE_INSTALL_FULL_LOCALSTATEDIR}/log/${_project_lower}" CACHE PATH "Directory of EDGESec log files")
-set(EDGESEC_local_lib_dir "${CMAKE_INSTALL_FULL_LOCALSTATEDIR}/lib/${_project_lower}" CACHE PATH "Directory of EDGESec persistant files (e.g. databases)")
-set(EDGESEC_runstate_dir "${CMAKE_INSTALL_FULL_RUNSTATEDIR}/${_project_lower}" CACHE PATH "Directory of EDGESec run-state files (.pid and socket files)")
-
-set(EDGESEC_cert_location "${EDGESEC_config_dir}/CA/CA.pem" CACHE FILEPATH "Path to edgesec certificate authority file")
 configure_file(
   "config.ini.in"
-  "${PROJECT_BINARY_DIR}/config.ini"
+  "config.ini"
   ESCAPE_QUOTES # values are quoted, so we need to escape quotes
   @ONLY # we only use @VAR_NAME@ syntax
 )
+configure_file(config.ini.in config.ini.in COPYONLY)
 
 # /etc/edgesec/config.ini folder
-install(FILES "${PROJECT_BINARY_DIR}/config.ini" DESTINATION "${EDGESEC_config_dir}")
+# runs configure_file again and install config.ini.in
+# install(SCRIPT "./CMakeModules/InstallConfigFile.cmake")
+install(CODE
+  "execute_process(
+    COMMAND ${CMAKE_COMMAND}
+      -D_project_lower=${_project_lower}
+      -DCMAKE_INSTALL_LIBDIR=${CMAKE_INSTALL_LIBDIR}
+      -Dbuild_dir=${CMAKE_BINARY_DIR}
+      -DCMAKE_INSTALL_PREFIX=\${CMAKE_INSTALL_PREFIX} # escape PREFIX so cmake --install --prefix works
+      -P ${CMAKE_SOURCE_DIR}/CMakeModules/InstallConfigFile.cmake
+  )"
+)
+
 get_filename_component(EDGESEC_cert_directory ${EDGESEC_cert_location} DIRECTORY)
 get_filename_component(EDGESEC_cert_filename ${EDGESEC_cert_location} NAME)
 install(FILES "${CMAKE_SOURCE_DIR}/scripts/deployments/rpi/CA.pem" DESTINATION "${EDGESEC_cert_directory}" RENAME "${EDGESEC_cert_filename}")
