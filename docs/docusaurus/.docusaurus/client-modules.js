e3c98904abf0b137d24d27003ecf51a231630fa3
Alexandru Mereacre <mereacre@gmail.com>
2021-12-09 16:49:46 +0000

    cleanup

--- docs/docusaurus/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/alexandru/Projects/EDGESec/docs/docusaurus/node_modules/infima/dist/css/default/default.css"),
-  require("/home/alexandru/Projects/EDGESec/docs/docusaurus/node_modules/@docusaurus/preset-classic/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/alexandru/Projects/EDGESec/docs/docusaurus/node_modules/@docusaurus/preset-classic/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/home/alexandru/Projects/EDGESec/docs/docusaurus/src/css/custom.css"),
-];
