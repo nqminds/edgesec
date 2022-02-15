95fac40c4df2b2d2cb96eac8f543918817efc87c
Alexandru Mereacre <mereacre@gmail.com>
2022-02-15 17:03:27 +0000

    cleanup

---
+++ src/utils/ifaceu.h
@@ -0,0 +1,35 @@
+/****************************************************************************
+ * Copyright (C) 2020 by NQMCyber Ltd                                       *
+ *                                                                          *
+ * This file is part of EDGESec.                                            *
+ *                                                                          *
+ *   EDGESec is free software: you can redistribute it and/or modify it     *
+ *   under the terms of the GNU Lesser General Public License as published  *
+ *   by the Free Software Foundation, either version 3 of the License, or   *
+ *   (at your option) any later version.                                    *
+ *                                                                          *
+ *   EDGESec is distributed in the hope that it will be useful,             *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *   GNU Lesser General Public License for more details.                    *
+ *                                                                          *
+ *   You should have received a copy of the GNU Lesser General Public       *
+ *   License along with EDGESec. If not, see <http://www.gnu.org/licenses/>.*
+ ****************************************************************************/
+
+/**
+ * @file ifaceu.h 
+ * @author Alexandru Mereacre
+ * @brief File containing the definition of the network interface utilities.
+ */
+
+#ifndef IFACEU_H_
+#define IFACEU_H_
+
+/**
+ * @brief if_nametoindex from net/if.h
+ * 
+ */
+unsigned int iface_nametoindex (const char *ifname);
+
+#endif
