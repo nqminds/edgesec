7e1e1d052cb803262bc5bbc81e964af4f1909bb8
Alois Klink <alois@nquiringminds.com>
2022-04-08 10:46:25 +0100

    Fix sqlite linker errors
    It looks like c611ce376b1e7e281bf214917272eb70f5cc937b now means
    sqlite is staticly linked.
    
    This means that -lpthread -lm (math.h) and -ldl need to be
    specified for compiling to work properly.

--- lib/sqlite.cmake
+++ lib/sqlite.cmake
@@ -1,4 +1,6 @@
 # Compile libsqlite
+include(CheckLibraryExists)
+
 if (BUILD_SQLITE_LIB AND NOT (BUILD_ONLY_DOCS))
   set(LIBSQLITE_INSTALL_ROOT "${CMAKE_CURRENT_BINARY_DIR}/lib")
   set(LIBSQLITE_INSTALL_DIR "${LIBSQLITE_INSTALL_ROOT}/sqlite")
@@ -24,4 +26,24 @@ if (BUILD_SQLITE_LIB AND NOT (BUILD_ONLY_DOCS))
     )
     find_library(LIBSQLITE_LIB NAMES libsqlite3.a sqlite3 PATHS "${LIBSQLITE_LIB_DIR}" NO_DEFAULT_PATH)
   endif ()
+
+  # static sqlite needs -lpthread -ldl -lm
+  if (NOT TARGET SQLite::sqlite)
+    add_library(SQLite::sqlite UNKNOWN IMPORTED)
+
+    set(sqlite_link_libs "pthread" "dl")
+
+    # Some compilers don't have -lm since it's always linked by default
+    CHECK_LIBRARY_EXISTS(m sin "" HAVE_LIB_M)
+    if (HAVE_LIB_M)
+      list(APPEND sqlite_link_libs "m")
+    endif(HAVE_LIB_M)
+
+
+    set_target_properties(SQLite::sqlite PROPERTIES
+        IMPORTED_LOCATION "${LIBSQLITE_LIB}"
+        INTERFACE_LINK_LIBRARIES "${sqlite_link_libs}"
+        INTERFACE_INCLUDE_DIRECTORIES "${LIBSQLITE_INCLUDE_DIR}"
+    )
+  endif(NOT TARGET SQLite::sqlite)
 endif ()
