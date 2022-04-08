8050ebf9ae09cca94f8c067a78771a1078bd5d07
Alois Klink <alois@nquiringminds.com>
2022-04-08 11:24:02 +0100

    Fix pcap linker errors
    It looks like c611ce376b1e7e281bf214917272eb70f5cc937b now means
    pcap is staticly linked.
    
    This means that -libverbs -lnl-genl-3 -lnl-3  -ldbus-1 needs to be
    specified for compiling to work properly.

--- lib/pcap.cmake
+++ lib/pcap.cmake
@@ -17,4 +17,19 @@ if (BUILD_PCAP_LIB AND NOT (BUILD_ONLY_DOCS))
     )
     find_library(LIBPCAP_LIB NAMES libpcap.a pcap PATHS "${LIBPCAP_LIB_DIR}" NO_DEFAULT_PATH)
   endif ()
+
+  # static pcap needs -libverbs -lnl-genl-3 -lnl-3  -ldbus-1
+  if (NOT TARGET PCAP::pcap)
+    add_library(PCAP::pcap UNKNOWN IMPORTED)
+
+    set(pcap_link_libs "ibverbs" "nl-genl-3" "nl-3" "dbus-1")
+
+
+    set_target_properties(PCAP::pcap PROPERTIES
+        IMPORTED_LOCATION "${LIBPCAP_LIB}"
+        INTERFACE_LINK_LIBRARIES "${pcap_link_libs}"
+        INTERFACE_INCLUDE_DIRECTORIES "${LIBPCAP_INCLUDE_PATH}"
+    )
+  endif(NOT TARGET PCAP::pcap)
+
 endif ()
