7a82e9c02789974fdae2076333da8fbfafec204f
Alois Klink <alois@nquiringminds.com>
2022-04-29 13:09:04 +0100

    build: Build libuuid using ExternalProject
    ExternalProject builds libuuid during build time, not configure time.

--- lib/uuid.cmake
+++ lib/uuid.cmake
@@ -3,27 +3,33 @@ if (BUILD_UUID_LIB AND NOT (BUILD_ONLY_DOCS))
 
   set(LIBUUID_INSTALL_ROOT "${CMAKE_CURRENT_BINARY_DIR}/lib")
   set(LIBUUID_INSTALL_DIR "${LIBUUID_INSTALL_ROOT}/uuid")
-  set(LIBUUID_INCLUDE_DIR "${LIBUUID_INSTALL_DIR}/include")
-  set(LIBUUID_LIB_DIR "${LIBUUID_INSTALL_DIR}/lib")
 
-  find_library(LIBUUID_LIB NAMES libuuid.a uuid PATHS "${LIBUUID_LIB_DIR}" NO_DEFAULT_PATH)
-  if (LIBUUID_LIB)
-    message("Found libuuid library: ${LIBUUID_LIB}")
-  ELSE ()
-    FetchContent_Declare(
-      util_linux
-      URL https://github.com/karelzak/util-linux/archive/refs/tags/v2.37.2.tar.gz
-      URL_HASH SHA3_256=d45c2b0ef0cca67ef9cbac1099503564d559fa1c52c0335dfd119546624b6bd0
-    )
+  message("Will install libuuid into ${LIBUUID_INSTALL_DIR}")
 
-    FetchContent_Populate(util_linux)
-    execute_process(COMMAND
-      bash
-      ${CMAKE_SOURCE_DIR}/lib/compile_uuid.sh
-      ${util_linux_SOURCE_DIR}
-      ${LIBUUID_INSTALL_ROOT}
-      ${target_autoconf_triple}
-    )
-    find_library(LIBUUID_LIB NAMES libuuid.a uuid PATHS "${LIBUUID_LIB_DIR}" NO_DEFAULT_PATH)
-  endif ()
+  # ExternalProject downloads/builds/installs at **build** time
+  # (e.g. during the `cmake --build` step)
+  ExternalProject_Add(
+    util_linux
+    URL https://github.com/karelzak/util-linux/archive/refs/tags/v2.37.2.tar.gz
+    URL_HASH SHA3_256=d45c2b0ef0cca67ef9cbac1099503564d559fa1c52c0335dfd119546624b6bd0
+    CONFIGURE_COMMAND ./autogen.sh
+    COMMAND ./configure
+      --with-pic=yes # -fPIC required since we want to link our static lib to sqlhook shared lib
+      --prefix=<INSTALL_DIR> --host=${target_autoconf_triple}
+      --disable-all-programs --enable-libuuid
+    BUILD_IN_SOURCE ON # not possible to build in seperate dir with autogen
+    INSTALL_DIR "${LIBUUID_INSTALL_DIR}"
+  )
+  ExternalProject_Get_Property(util_linux INSTALL_DIR)
+
+  set(LIBUUID_LIB_DIR "${INSTALL_DIR}/lib")
+  set(LIBUUID_INCLUDE_DIR "${INSTALL_DIR}/include")
+
+  add_library(util_linux::uuid STATIC IMPORTED)
+  file(MAKE_DIRECTORY "${LIBUUID_INCLUDE_DIR}")
+  set_target_properties(util_linux::uuid PROPERTIES
+    IMPORTED_LOCATION "${LIBUUID_LIB_DIR}/libuuid.a"
+    INTERFACE_INCLUDE_DIRECTORIES "${LIBUUID_INCLUDE_DIR}"
+  )
+  add_dependencies(util_linux::uuid util_linux)
 endif ()
