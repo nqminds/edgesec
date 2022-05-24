ec4b5428a110cd50a8ecf30fe91f02cf004a5f73
Alois Klink <alois@nquiringminds.com>
2022-05-24 16:09:41 +0100

    build(uuid): build uuid in own BUILD_DIR
    Previously, we were building in the util_linux SOURCE_DIR,
    which is not ideal.

--- lib/uuid.cmake
+++ lib/uuid.cmake
@@ -12,12 +12,11 @@ if (BUILD_UUID_LIB AND NOT (BUILD_ONLY_DOCS))
     util_linux
     URL https://github.com/karelzak/util-linux/archive/refs/tags/v2.37.2.tar.gz
     URL_HASH SHA3_256=d45c2b0ef0cca67ef9cbac1099503564d559fa1c52c0335dfd119546624b6bd0
-    CONFIGURE_COMMAND ./autogen.sh
-    COMMAND ./configure
+    CONFIGURE_COMMAND <SOURCE_DIR>/autogen.sh
+    COMMAND <SOURCE_DIR>/configure
       --with-pic=yes # -fPIC required since we want to link our static lib to sqlhook shared lib
       --prefix=<INSTALL_DIR> --host=${target_autoconf_triple}
       --disable-all-programs --enable-libuuid
-    BUILD_IN_SOURCE ON # not possible to build in seperate dir with autogen
     INSTALL_DIR "${LIBUUID_INSTALL_DIR}"
   )
   ExternalProject_Get_Property(util_linux INSTALL_DIR)
