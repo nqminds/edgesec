eb9da9cea8ecd9f2d0517fe24ea164c4d7ca4dcb
Alois Klink <alois@nquiringminds.com>
2022-05-30 12:58:28 +0100

    build(hostapd): build hostap with externalproject
    Build hostapd with CMake's ExternalProject module.
    
    This builds hostapd at compile time, instead of at configure time.
    This allows multiprocessing.

--- lib/hostapd.cmake
+++ lib/hostapd.cmake
@@ -1,24 +1,21 @@
-# Find the hostapd program
+# Builds hostapd using ExternalProject
+
+# v3.14.0+ is required by BUILD_IN_SOURCE + SOURCE_SUBDIR together
+cmake_minimum_required(VERSION 3.14.0)
+include(ExternalProject)
+
 if (BUILD_HOSTAPD AND NOT (BUILD_ONLY_DOCS))
-  set(HOSTAPD_SOURCE_DIR "${CMAKE_SOURCE_DIR}/lib/hostap/hostapd")
+  set(HOSTAPD_SOURCE_DIR "${CMAKE_SOURCE_DIR}/lib/hostap")
   set(HOSTAPD_INSTALL_DIR "${CMAKE_CURRENT_BINARY_DIR}")
-  find_program(HOSTAPD NAMES hostapd PATHS "${HOSTAPD_INSTALL_DIR}" NO_DEFAULT_PATH)
-  if (HOSTAPD)
-    message("Found hostapd program: ${HOSTAPD}")
-  ELSE ()
-    message("Building hostapd...")
-    execute_process(
-      COMMAND make
-      WORKING_DIRECTORY "${HOSTAPD_SOURCE_DIR}"
-    )
-    execute_process(
-      COMMAND cp ./hostapd "${HOSTAPD_INSTALL_DIR}"
-      WORKING_DIRECTORY "${HOSTAPD_SOURCE_DIR}"
-    )
-    execute_process(
-      COMMAND make clean
-      WORKING_DIRECTORY "${HOSTAPD_SOURCE_DIR}"
-    )
-    find_program(HOSTAPD NAMES hostapd PATHS "${HOSTAPD_INSTALL_DIR}" NO_DEFAULT_PATH)
-  endif ()
+
+  ExternalProject_Add(
+    hostapd_externalproject
+    URL "${HOSTAPD_SOURCE_DIR}"
+    INSTALL_DIR "${HOSTAPD_INSTALL_DIR}"
+    BUILD_IN_SOURCE true
+    SOURCE_SUBDIR "hostapd" # we only care about hostapd, not the entire hostap dir
+    CONFIGURE_COMMAND "" # no configure command
+    INSTALL_COMMAND cmake -E copy <BINARY_DIR>/hostapd <INSTALL_DIR>/hostapd
+  )
+  set(HOSTAPD "${HOSTAPD_INSTALL_DIR}/hostapd")
 endif ()
