5e597d24898d6b5d1875dc7c1b87e91fa7eabe99
Alois Klink <alois@nquiringminds.com>
2022-06-24 14:23:33 +0100

    build: add edgesecAddCaptureMiddleware function
    Adds a CMake function called `edgesecAddCaptureMiddleware`
    that allows users of EDGESec to create their own capture middlewares.
    
    Users just need to declare a CMake library that contains
    a `struct capture_middleware` that contains function pointers
    to the `init()`, `process()`, and `free()` functions for their
    middleware.
    
    Then, they can use the following for EDGESec to automatically
    link their middleware to the capture service.
    
    ```cmake
       edgesecAddCaptureMiddleware(
            MIDDLEWARE_TARGET <CMAKE_TARGET_CONTAINING_YOUR_MIDDLEWARE>
            MIDDLEWARE_STRUCT <C_NAME_OF_MIDDLEWARE_VARIABLE>
       )
    ```

---
+++ src/capture/middlewares.h
@@ -0,0 +1,58 @@
+#include <sqlite3.h>
+#include <pcap.h>
+#include "middleware.h"
+#include "../utils/eloop.h"
+
+struct middleware_handlers {
+  const struct capture_middleware f;
+  struct middleware_context *context;
+};
+
+static const UT_icd middleware_icd = {sizeof(struct middleware_handlers), NULL,
+                                      NULL, NULL};
+
+UT_array *assign_middlewares(void);
+
+static inline int init_middlewares(UT_array *handlers, sqlite3 *db,
+                                   char *db_path, struct eloop_data *eloop,
+                                   struct pcap_context *pc) {
+  struct middleware_handlers *handler = NULL;
+
+  while ((handler =
+              (struct middleware_handlers *)utarray_next(handlers, handler))) {
+    log_trace("Initialising capture middleware: %s", handler->f.name);
+    handler->context = handler->f.init(db, db_path, eloop, pc);
+    if (handler->context == NULL) {
+      log_error("handle init error");
+      return -1;
+    }
+  }
+
+  return 0;
+}
+
+static inline void free_middlewares(UT_array *handlers) {
+  struct middleware_handlers *handler = NULL;
+
+  while ((handler =
+              (struct middleware_handlers *)utarray_next(handlers, handler))) {
+    handler->f.free(handler->context);
+    handler->context = NULL;
+  }
+
+  utarray_free(handlers);
+}
+
+static inline void process_middlewares(UT_array *handlers, char *ltype,
+                                       struct pcap_pkthdr *header,
+                                       uint8_t *packet, char *ifname) {
+  struct middleware_handlers *handler = NULL;
+
+  while ((handler =
+              (struct middleware_handlers *)utarray_next(handlers, handler))) {
+    if (handler->f.process(handler->context, ltype, header, packet, ifname) <
+        0) {
+      log_error("handler process fail");
+    }
+  }
+}
