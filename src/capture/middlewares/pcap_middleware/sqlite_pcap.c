6e5a9b49106790bd7e10ded858704737d55c1ff7
Alois Klink <alois@nquiringminds.com>
2022-06-27 18:30:08 +0100

    docs: compress the license header in source files
    Replaces the super verbose license message with a much more
    compressed version that uses the SPDX ID standard:
    https://spdx.org/licenses/
    
    This is what the Linux kernel uses: https://lwn.net/Articles/739183/
    
    This commit was created by running the following regex command:
    Input regex:
        \/\*{76}\n.* (\d+) by NQMCyber Ltd[\w\W]*GNU Lesser[\w\W]*\*{76}\/
    Output string (where `$1` is the first capture group)
        /**
         * @file
         * @author Alexandru Mereacre
         * @date $1
         * @copyright
         * SPDX-FileCopyrightText: © $1 NQMCyber Ltd and edgesec contributors
         * SPDX-License-Identifier: LGPL-3.0-or-later
         */

--- src/capture/middlewares/pcap_middleware/sqlite_pcap.c
+++ src/capture/middlewares/pcap_middleware/sqlite_pcap.c
@@ -1,21 +1,11 @@
-/****************************************************************************
- * Copyright (C) 2021 by NQMCyber Ltd                                       *
- *                                                                          *
- * This file is part of EDGESec.                                            *
- *                                                                          *
- *   EDGESec is free software: you can redistribute it and/or modify it     *
- *   under the terms of the GNU Lesser General Public License as published  *
- *   by the Free Software Foundation, either version 3 of the License, or   *
- *   (at your option) any later version.                                    *
- *                                                                          *
- *   EDGESec is distributed in the hope that it will be useful,             *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
- *   GNU Lesser General Public License for more details.                    *
- *                                                                          *
- *   You should have received a copy of the GNU Lesser General Public       *
- *   License along with EDGESec. If not, see <http://www.gnu.org/licenses/>.*
- ****************************************************************************/
+/**
+ * @file
+ * @author Alexandru Mereacre
+ * @date 2021
+ * @copyright
+ * SPDX-FileCopyrightText: © 2021 NQMCyber Ltd and edgesec contributors
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
 
 /**
  * @file sqlite_pcap.c
