1a48f20afc61a696b714004e5051d7b94e7ca54b
Alois Klink <alois@nquiringminds.com>
2022-06-27 17:55:50 +0100

    build(docs): improve build when -DBUILD_ONLY_DOCS
    Throws an error if doxygen does not exist if -DBUILD_ONLY_DOCS=ON

--- docs/doxygen.cmake
+++ docs/doxygen.cmake
@@ -1,10 +1,14 @@
 cmake_minimum_required(VERSION 3.9.0) # required by FindDoxygen.cmake
 
 # check if Doxygen is installed
-find_package(Doxygen OPTIONAL_COMPONENTS dot)
+if (BUILD_ONLY_DOCS)
+  find_package(Doxygen REQUIRED dot)
+else ()
+  find_package(Doxygen OPTIONAL_COMPONENTS dot)
+endif()
 
 if (DOXYGEN_FOUND)
-    if (NOT DEFINED Doxygen::dot)
+    if (NOT TARGET Doxygen::dot)
         message(
           WARNING
           "dot is not installed, but is highly recommended to create directed graphs"
@@ -15,19 +19,23 @@ if (DOXYGEN_FOUND)
     # set(DOXYGEN_IMAGE_PATH "${PROJECT_SOURCE_DIR}/docs")
     # currently unused, set if we want to use the `@dotfile` command
     # set(DOXYGEN_DOTFILE_DIRS "${PROJECT_SOURCE_DIR}/docs")
-    set(DOXYGEN_OUTPUT_DIRECTORY "${PROJECT_SOURCE_DIR}/docs")
-    # the docusaurus folder has a node_modules folder, which is a bunch of mess
-    set(DOXYGEN_EXCLUDE_PATTERNS "${PROJECT_SOURCE_DIR}/docs/docusaurus")
+    # set(DOXYGEN_OUTPUT_DIRECTORY "${PROJECT_SOURCE_DIR}/docs")
 
     set(DOXYGEN_PROJECT_NAME "edgesec") # defaults to EDGESEC
     set(DOXYGEN_EXTRACT_ALL YES) # document even files missing `@file` command
 
-    # note the option ALL which allows to build the docs together with the application
-    doxygen_add_docs(doxydocs
-      "${PROJECT_SOURCE_DIR}/src" ${PROJECT_SOURCE_DIR}/docs
-      # ALL
-      COMMENT "Generating API documentation with Doxygen"
-    )
+    if (BUILD_ONLY_DOCS)
+      doxygen_add_docs(doxydocs
+        "${PROJECT_SOURCE_DIR}/src" "${PROJECT_SOURCE_DIR}/docs"
+        ALL # part of make all
+        COMMENT "Generating API documentation with Doxygen"
+      )
+    else ()
+      doxygen_add_docs(doxydocs
+        "${PROJECT_SOURCE_DIR}/src" "${PROJECT_SOURCE_DIR}/docs"
+        COMMENT "Generating API documentation with Doxygen"
+      )
+    endif()
 else ()
   message(WARNING "Doxygen need to be installed to generate the doxygen documentation")
 endif ()
