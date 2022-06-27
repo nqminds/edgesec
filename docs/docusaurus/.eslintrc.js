ada8912e97d8f039ab4eeb815863dd98da092845
Alex <mereacre@gmail.com>
2022-06-27 15:49:24 +0100

    moved docusaurus to separate repo

--- docs/docusaurus/.eslintrc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  "extends": [
-    "@nqminds/eslint-config-react",
-  ],
-};
--- docs/docusaurus/sidebars.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  someSidebar: {
-    Introduction: ["intro", "control", "capture", "discovery", "storage"],
-    Development: ["compilation", "running", "config", "commands", "deb", "issues"],
-    Deployment: ["installation"],
-    Hardware: ["devices"],
-  },
-};
