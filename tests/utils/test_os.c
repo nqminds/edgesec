d3076b202a5a3d65ff4187d5270326806fdb18ba
Alois Klink <alois@nquiringminds.com>
2022-08-22 15:57:37 +0100

    test: test make_dirs_to_path error branches
    Test whether make_dirs_to_path correctly throws an error when:
      - an invalid input string is passed (not a string)
      - an directory cannot be made
        - ENOTDIR (the path contains a non-directory), is the only
          error condition we can easily induce. All the other mkdir()
          error codes require root Linux access.

--- tests/utils/test_os.c
+++ tests/utils/test_os.c
@@ -558,6 +558,23 @@ static void test_make_dirs_to_path(void **state) {
   // should return no error code
   assert_int_equal(ret, 0);
 
+  // should return an error on invalid input string
+  ret = make_dirs_to_path(NULL, 0755);
+  assert_int_equal(ret, -1);
+
+  // create a file in the directory
+  FILE *fp = fopen(path, "w");
+  assert_non_null(fp);
+  assert_int_not_equal(fputs("test file, should be deleted", fp), EOF);
+  assert_int_not_equal(fclose(fp), EOF);
+
+  // should throw a ENOTDIR (NOT A DIRECTORY) error when trying to create
+  // folder in `not_a_dir.txt`
+  const *enotdir_path = construct_path(directories_to_build,
+                                       "not_a_dir.txt/new_folder/new_file.txt");
+  assert_int_equal(make_dirs_to_path(enotdir_path, 0755), -1);
+  free(enotdir_path);
+
   free(directories_to_build);
   free(path);
 }
