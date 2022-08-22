nqminds/edgesec

commit cbb999b28fbc7f30de2e3237074821ea9e91163e

wip: cleanup processes

src/utils/os.c
@@ -542,7 +542,12 @@ int list_dir(char *dirpath, list_dir_fn fun, void *args)
 
     /* Print directory + filename */
     char *path = construct_path(dirpath, dp->d_name);
-    fun(path, args);
+    if (!fun(path, args)) {
+      log_trace("list_dir callback fail");
+      os_free(path);
+      goto exit_list_dir;
+    }
+
     os_free(path);
   }
 
@@ -551,10 +556,59 @@ int list_dir(char *dirpath, list_dir_fn fun, void *args)
     return -1;
   }
 
+exit_list_dir:
   if (closedir(dirp) == -1) {
     log_err("closedir");
     return -1;
   }
 
   return 0;
+}
+
+long is_app(char *path, char *proc_name)
+{
+  char exe_path[MAX_OS_PATH_LEN];
+  char resolved_path[MAX_OS_PATH_LEN];
+
+  unsigned long pid = strtoul(basename(path), NULL, 10);
+
+  if (errno != ERANGE && pid != 0L) {
+    snprintf(exe_path, MAX_OS_PATH_LEN - 1, "%s/exe", path);
+    if (realpath(exe_path, resolved_path) != NULL) {
+      if (strcmp(basename(resolved_path), proc_name) == 0) {
+        return pid;
+      }
+    }
+  }
+
+  return 0;
+}
+
+bool kill_dir_fn(char *path, void *args)
+{
+  unsigned long pid;
+  pid_t current_pid = getpid();
+
+  if ((pid = is_app(path, args)) != 0) {
+    if (current_pid != pid) {
+      if (kill(pid, SIGKILL) == -1) {
+        log_err("kill");
+        return false;
+      } else
+        log_trace("killed %s process with pid=%d", args, pid);
+    }
+  }
+
+  return true;
+}
+
+bool kill_process(char *proc_name)
+{
+  // Kill a process process if running
+  if (list_dir("/proc", kill_dir_fn, proc_name) == -1) {
+    log_trace("list_dir fail");
+    return false;
+  }
+
+  return true;
 }

src/utils/os.h
@@ -364,7 +364,7 @@ char *construct_path(char *path_left, char *path_right);
  */
 char* get_secure_path(UT_array *bin_path_arr, char *filename, char *filehash);
 
-typedef void(*list_dir_fn)(char *, void *args);
+typedef bool(*list_dir_fn)(char *, void *args);
 
 /**
  * @brief List the files in a directory
@@ -376,4 +376,11 @@ typedef void(*list_dir_fn)(char *, void *args);
  */
 int list_dir(char *dirpath, list_dir_fn fun, void *args);
 
+/**
+ * @brief Kill a process by name
+ * 
+ * @param proc_name The process name
+ * @return bool true on success, false otherwise 
+ */
+bool kill_process(char *proc_name);
 #endif /* OS_H */

config.ini.in
@@ -1,13 +1,14 @@
 [system]
 binPath = "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin"
 hashIpCommand = ""
-createInterfaces = false
-ignoreErrorOnIfCreate = false
+createInterfaces = true
+ignoreErrorOnIfCreate = true
 allowAllConnections = false
 apDetect = false
-execHostapd = true
 defaultOpenVlanId = 0
-execRadius = false
+execHostapd = true
+execRadius = true
+execDhcp = true
 
 [supervisor]
 domainServerPath = /tmp/edgesec-domain-server
@@ -51,6 +52,13 @@ secret = "radius"
 [nat]
 natInterface = "enp2s0"
 
+[dns]
+servers="8.8.4.4,8.8.8.8"
+
+[dhcp]
+dhcpConfigPath = "/tmp/dnsmasq.conf"
+dhcpScriptPath = "/tmp/dnsmasq_exec.sh"
+
 [connections]
 con1 = "d,04:f0:21:5a:f4:c4,0,1,1234554321"
 con2 = "d,30:52:cb:e9:00:8f,1,1,1234554321"
@@ -59,7 +67,7 @@ con4 = "d,60:70:c0:0a:23:ba,3,1,1234554321"
 con5 = "d,00:0f:00:70:62:88,4,1,1234554321"
 con6 = "d,9c:ef:d5:fd:db:56,5,1,1234554321"
 con7 = "d,c0:ee:fb:d5:5a:ec,6,1,1234554321"
-con8 = "a,00:0f:60:0b:6a:07,7,1,1234554321"
+con8 = "a,00:0f:00:70:62:88,7,1,1234554321"
 
 [interfaces]
 subnetMask = "255.255.255.0"

src/dhcp/CMakeLists.txt
@@ -1,0 +1,3 @@
+include_directories (
+  "${EDGESEC_SOURCE_DIR}/src"
+)

src/dhcp/dhcp_service.h
@@ -1,0 +1,41 @@
+/****************************************************************************
+ * Copyright (C) 2021 by NQMCyber Ltd                                       *
+ *                                                                          *
+ * This file is part of EDGESec.                                            *
+ *                                                                          *
+ *   EDGESec is free software: you can redistribute it and/or modify it     *
+ *   under the terms of the GNU Lesser General Public License as published  *
+ *   by the Free Software Foundation, either version 3 of the License, or   *
+ *   (at your option) any later version.                                    *
+ *                                                                          *
+ *   EDGESec is distributed in the hope that it will be useful,             *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *   GNU Lesser General Public License for more details.                    *
+ *                                                                          *
+ *   You should have received a copy of the GNU Lesser General Public       *
+ *   License along with EDGESec. If not, see <http://www.gnu.org/licenses/>.*
+ ****************************************************************************/
+
+/**
+ * @file dhcp_service.h
+ * @author Alexandru Mereacre 
+ * @brief File containing the definition of dhcp service configuration utilities.
+ */
+#ifndef DHCP_SERVICE_H
+#define DHCP_SERVICE_H
+
+#include "../utils/os.h"
+#include "../utils/utarray.h"
+
+
+/**
+ * @brief The dhcp configuration structures.
+ * 
+ */
+struct dhcp_conf {
+  char dhcp_conf_path[MAX_OS_PATH_LEN];                 /**< The dhcp config path string */
+  char dhcp_script_path[MAX_OS_PATH_LEN];               /**< The dhcp executable script path string */
+};
+
+#endif

src/dns/CMakeLists.txt
@@ -1,0 +1,3 @@
+include_directories (
+  "${EDGESEC_SOURCE_DIR}/src"
+)

src/dns/dns_service.h
@@ -1,0 +1,39 @@
+/****************************************************************************
+ * Copyright (C) 2021 by NQMCyber Ltd                                       *
+ *                                                                          *
+ * This file is part of EDGESec.                                            *
+ *                                                                          *
+ *   EDGESec is free software: you can redistribute it and/or modify it     *
+ *   under the terms of the GNU Lesser General Public License as published  *
+ *   by the Free Software Foundation, either version 3 of the License, or   *
+ *   (at your option) any later version.                                    *
+ *                                                                          *
+ *   EDGESec is distributed in the hope that it will be useful,             *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *   GNU Lesser General Public License for more details.                    *
+ *                                                                          *
+ *   You should have received a copy of the GNU Lesser General Public       *
+ *   License along with EDGESec. If not, see <http://www.gnu.org/licenses/>.*
+ ****************************************************************************/
+
+/**
+ * @file dns_service.h 
+ * @author Alexandru Mereacre 
+ * @brief File containing the definition of dns service configuration utilities.
+ */
+#ifndef DNS_SERVICE_H
+#define DNS_SERVICE_H
+
+#include "../utils/utarray.h"
+
+
+/**
+ * @brief The dns configuration structures.
+ * 
+ */
+struct dns_conf {
+  UT_array *server_array;                      /**< The array including the DNS servers IP addresses. */
+};
+
+#endif

src/edgesec.c
@@ -33,6 +33,7 @@
 #include <stdbool.h>
 #include <errno.h>
 #include <net/if.h>
+#include <libgen.h>
 
 #include "version.h"
 #include "utils/log.h"
@@ -285,7 +286,7 @@ bool load_connection_list(const char *filename, struct app_config *config)
   os_free(key);
   return true;
 }
-void load_radius_conf(const char *filename, struct app_config *config)
+bool load_radius_conf(const char *filename, struct app_config *config)
 {
   char *value = os_malloc(INI_BUFFERSIZE);
 
@@ -294,7 +295,8 @@ void load_radius_conf(const char *filename, struct app_config *config)
   int ret = ini_gets("hostapd", "ssid", "", value, INI_BUFFERSIZE, filename);
   if (!ret) {
     fprintf(stderr, "hostapd ssid was not specified\n");
-    exit(1);
+    os_free(value);
+    return false;
   }
 
   strncpy(config->hconfig.ssid, value, HOSTAPD_AP_NAME_LEN);
@@ -312,7 +314,8 @@ void load_radius_conf(const char *filename, struct app_config *config)
   ret = ini_gets("hostapd", "interface", "", value, INI_BUFFERSIZE, filename);
   if (!ret) {
     fprintf(stderr, "AP interface was not specified\n");
-    exit(1);
+    os_free(value);
+    return false;
   }
 
   strncpy(config->hconfig.interface, value, IFNAMSIZ);
@@ -346,6 +349,8 @@ void load_radius_conf(const char *filename, struct app_config *config)
   ini_gets("radius", "secret", "", value, INI_BUFFERSIZE, filename);
   strncpy(config->rconfig.radius_secret, value, RADIUS_SECRET_LEN);
   os_free(value);
+
+  return true;
 }
 
 bool load_hostapd_conf(const char *filename, struct app_config *config)
@@ -356,7 +361,8 @@ bool load_hostapd_conf(const char *filename, struct app_config *config)
   int ret = ini_gets("hostapd", "hostapdFilePath", "", value, INI_BUFFERSIZE, filename);
   if (!ret) {
     fprintf(stderr, "hostapd file path was not specified\n");
-    exit(1);
+    os_free(value);
+    return false;
   }
 
   strncpy(config->hconfig.hostapd_file_path, value, MAX_OS_PATH_LEN);
@@ -367,7 +373,8 @@ bool load_hostapd_conf(const char *filename, struct app_config *config)
   ret = ini_gets("hostapd", "hostapdBinPath", "", value, INI_BUFFERSIZE, filename);
   if (!ret) {
     fprintf(stderr, "hostapd hostapdBinPath was not specified\n");
-    exit(1);
+    os_free(value);
+    return false;
   }
 
   strncpy(config->hconfig.hostapd_bin_path, value, MAX_OS_PATH_LEN);
@@ -384,7 +391,8 @@ bool load_hostapd_conf(const char *filename, struct app_config *config)
   ret = ini_gets("hostapd", "bridge", "", value, INI_BUFFERSIZE, filename);
   if (!ret) {
     fprintf(stderr, "hostapd bridge was not specified\n");
-    exit(1);
+    os_free(value);
+    return false;
   }
 
   strncpy(config->hconfig.bridge, value, IFNAMSIZ);
@@ -443,7 +451,8 @@ bool load_hostapd_conf(const char *filename, struct app_config *config)
   ret = ini_gets("hostapd", "vlanBridge", "", value, INI_BUFFERSIZE, filename);
   if (!ret) {
     fprintf(stderr, "hostapd vlanBridge was not specified\n");
-    exit(1);
+    os_free(value);
+    return false;
   }
 
   strncpy(config->hconfig.vlan_bridge, value, IFNAMSIZ);
@@ -454,7 +463,8 @@ bool load_hostapd_conf(const char *filename, struct app_config *config)
   ret = ini_gets("hostapd", "vlanFile", "", value, INI_BUFFERSIZE, filename);
   if (!ret) {
     fprintf(stderr, "hostapd vlanFile was not specified\n");
-    exit(1);
+    os_free(value);
+    return false;
   }
 
   strncpy(config->hconfig.vlan_file, value, MAX_OS_PATH_LEN);
@@ -477,6 +487,48 @@ bool load_hostapd_conf(const char *filename, struct app_config *config)
 
   // Load hostapd wpaPskRadius
   config->hconfig.wpa_psk_radius = (int) ini_getl("hostapd", "wpaPskRadius", 2, filename);
+
+  return true;
+}
+
+bool load_dns_conf(const char *filename, struct app_config *config)
+{
+  char *value = os_malloc(INI_BUFFERSIZE);
+
+  // Load the DNS server addresses
+  ini_gets("system", "binPath", "", value, INI_BUFFERSIZE, filename);
+  split_string_array(value, ',', config->dns_config.server_array);
+  os_free(value);
+
+  return true;
+}
+
+bool load_dhcp_conf(const char *filename, struct app_config *config)
+{
+  char *value = os_malloc(INI_BUFFERSIZE);
+
+  // Load dhpc config file path
+  int ret = ini_gets("dhcp", "dhcpConfigPath", "", value, INI_BUFFERSIZE, filename);
+  if (!ret) {
+    fprintf(stderr, "dhcp dhcpConfigPath was not specified\n");
+    os_free(value);
+    return false;
+  }
+
+  strncpy(config->dhcp_config.dhcp_conf_path, value, MAX_OS_PATH_LEN);
+  os_free(value);
+
+  // Load dhpc script file path
+  value = os_malloc(INI_BUFFERSIZE);
+  ret = ini_gets("dhcp", "dhcpScriptPath", "", value, INI_BUFFERSIZE, filename);
+  if (!ret) {
+    fprintf(stderr, "dhcp dhcpScriptPath was not specified\n");
+    os_free(value);
+    return false;
+  }
+
+  strncpy(config->dhcp_config.dhcp_script_path, value, MAX_OS_PATH_LEN);
+  os_free(value);
 
   return true;
 }
@@ -511,6 +563,9 @@ void load_app_config(const char *filename, struct app_config *config)
 
   // Load the exec radius flag
   config->exec_radius = ini_getbool("system", "execRadius", 0, filename);
+
+  // Load the exec dhcp flag
+  config->exec_dhcp = ini_getbool("system", "execDhcp", 0, filename);
 
   // Load the default open vlanid
   config->default_open_vlanid = (int) ini_getl("system", "defaultOpenVlanId", 0, filename);
@@ -560,13 +615,30 @@ void load_app_config(const char *filename, struct app_config *config)
   }
 
   // Load hostapd radius config params
-  load_radius_conf(filename, config);
+  if(!load_radius_conf(filename, config)) {
+    fprintf(stderr, "radius config parsing error.\n");
+    exit(1);
+  }
 
   // Load hostapd config params
   if(!load_hostapd_conf(filename, config)) {
-    fprintf(stderr, "hostapd confif parsing error.\n");
+    fprintf(stderr, "hostapd config parsing error.\n");
     exit(1);
   }
+
+  if(!load_dns_conf(filename, config)) {
+    fprintf(stderr, "dns config parsing error.\n");
+    exit(1);
+  }
+
+  if(!load_dhcp_conf(filename, config)) {
+    fprintf(stderr, "dhcp config parsing error.\n");
+    exit(1);
+  }
+}
+
+char *get_app_name(char *app_path) {
+  return basename(app_path);
 }
 
 int main(int argc, char *argv[])
@@ -577,6 +649,7 @@ int main(int argc, char *argv[])
   UT_array *bin_path_arr;
   UT_array *config_ifinfo_arr;
   UT_array *mac_conn_arr;
+  UT_array *server_arr;
   struct app_config config;
 
   // Create the empty dynamic array for bin path strings
@@ -591,6 +664,10 @@ int main(int argc, char *argv[])
   utarray_new(mac_conn_arr, &mac_conn_icd);
   config.connections = mac_conn_arr;
 
+  // Create the dns server array
+  utarray_new(server_arr, &ut_str_icd);
+  config.dns_config.server_array = server_arr;
+
   process_app_options(argc, argv, &verbosity, &filename);
 
   if (verbosity > MAX_LOG_LEVELS) {
@@ -604,6 +681,12 @@ int main(int argc, char *argv[])
   if (optind <= 1) show_app_help(argv[0]);
 
   load_app_config(filename, &config);
+
+  // Kill all edgesec processes if running
+  if(!kill_process(get_app_name(argv[0]))){
+    exit(1);
+  }
+
   if (!run_engine(&config, level)) {
     fprintf(stderr, "Failed to start edgesec engine.\n");
   } else
@@ -612,5 +695,6 @@ int main(int argc, char *argv[])
   utarray_free(bin_path_arr);
   utarray_free(config_ifinfo_arr);
   utarray_free(mac_conn_arr);
+  utarray_free(server_arr);
   exit(0);
 }

commit d228fa0ab65dcf6772eeaf03500d11fa88f0bcc5

fix: return -1 in `list_dir` if `dir_fn` fails
Currently, if `dir_fn` ever returns `false`, `list_dir` will
exit early, but still return `0` (success).

This seems incorrect to me, so this commit changes it to return -1
when `dir_fn` fails.

Fixes: cbb999b28fbc7f30de2e3237074821ea9e91163e

src/utils/os.c
@@ -679,6 +679,8 @@ int list_dir(char *dirpath, list_dir_fn fun, void *args) {
     return -1;
   }
 
+  int returnValue = 0;
+
   /* Look at each of the entries in this directory */
   for (;;) {
     errno = 0; /* To distinguish error from end-of-directory */
@@ -696,6 +698,7 @@ int list_dir(char *dirpath, list_dir_fn fun, void *args) {
       if (!fun(path, args)) {
         log_trace("list_dir callback fail");
         os_free(path);
+        returnValue = -1;
         goto exit_list_dir;
       }
     }
@@ -714,7 +717,7 @@ exit_list_dir:
     return -1;
   }
 
-  return 0;
+  return returnValue;
 }
 
 bool is_string_in_cmdline_file(char *filename, char *str) {

src/utils/os.h
@@ -375,8 +375,10 @@ typedef bool (*list_dir_fn)(char *, void *args);
  *
  * @param dirpath The directory path
  * @param fun The callback function
+ *            If this function ever returns `false`, list_dir will return `-1`.
  * @param args The callback function arguments
- * @return int
+ * @retval  0 On success
+ * @retval -1 On error
  */
 int list_dir(char *dirpath, list_dir_fn fun, void *args);
 

tests/utils/test_os.c
@@ -465,6 +465,12 @@ bool dir_fn(char *dirpath, void *args) {
   return true;
 }
 
+bool failing_dir_fn(char *dirpath, void *args) {
+  (void)dirpath;
+  (void)args;
+  return false;
+}
+
 static void test_list_dir(void **state) {
   (void)state;
 
@@ -472,6 +478,12 @@ static void test_list_dir(void **state) {
   int ret = list_dir("/bin", dir_fn, &is_uname);
   assert_int_equal(ret, 0);
   assert_int_equal(is_uname, 1);
+
+  // should fail for invalid folder
+  assert_int_equal(list_dir("/this-path-is-not-a-dir", dir_fn, &is_uname), -1);
+
+  // should fail if dir_fn fails
+  assert_int_equal(list_dir("/bin", failing_dir_fn, &is_uname), -1);
 }
 
 typedef struct {