fbaee5ef337ff6281f54345723bffef0a2ab6bc5
Alois Klink <alois@nquiringminds.com>
2022-09-02 12:46:25 +0100

    refactor: remove unnecessary middlewares includes
    Including `<netinet/ip.h>` is causing issues on FreeBSD.
    These includes aren't actually needed, so we might as well get rid of
    them and speed up compilation slightly.

--- src/capture/middlewares/header_middleware/mdns_decoder.c
+++ src/capture/middlewares/header_middleware/mdns_decoder.c
@@ -9,15 +9,7 @@
  * utilities.
  */
 
-#include <netinet/in.h>
-#include <netinet/ip.h>
-#include <netinet/if_ether.h>
-#include <netinet/ip6.h>
-#include <netinet/icmp6.h>
-#include <netinet/ip_icmp.h>
-#include <netinet/tcp.h>
 #include <netinet/udp.h>
-#include <arpa/inet.h>
 
 #include "../../../utils/allocs.h"
 #include "../../../utils/os.h"
