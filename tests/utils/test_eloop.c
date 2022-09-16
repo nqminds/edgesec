f6017c7dc434706cafbfedc8ecfc300f7e1120c3
Alois Klink <alois@nquiringminds.com>
2022-09-16 10:04:54 +0100

    test(eloop): test all eloop_*_timeout fuctions
    Adds a basic test that tests every eloop_*_timeout function.

--- tests/utils/test_eloop.c
+++ tests/utils/test_eloop.c
@@ -63,6 +63,163 @@ static void test_eloop_init(void **state) {
   eloop_free(eloop);
 }
 
+struct test_state_t {
+  struct eloop_data *eloop;
+};
+
+int setup(void **state) {
+  struct test_state_t *test_state = calloc(1, sizeof(struct test_state_t));
+  test_state->eloop = eloop_init();
+  assert_non_null(test_state->eloop);
+  *state = test_state;
+  return 0;
+}
+
+int teardown(void **state) {
+  struct test_state_t *test_state = *state;
+  eloop_free(test_state->eloop);
+  free(test_state);
+  return 0;
+}
+
+static void eloop_timeout_handler_function(void *eloop_ctx, void *user_ctx) {
+  check_expected(eloop_ctx);
+  check_expected(user_ctx);
+}
+
+/**
+ * @brief Should call eloop timeouts in order of timeout
+ *
+ * @param state CMocka test state created with setup() and closed with
+ * teardown()
+ */
+static void test_eloop_timeout(void **state) {
+  struct test_state_t *test_state = *state;
+
+  char eloop_data[] = "this is eloop data";
+  char user_data[] = "user data";
+
+  // check if timeout is registered
+  assert_false(eloop_is_timeout_registered(test_state->eloop,
+                                           eloop_timeout_handler_function,
+                                           eloop_data, user_data));
+  expect_string(eloop_timeout_handler_function, eloop_ctx, eloop_data);
+  expect_string(eloop_timeout_handler_function, user_ctx, user_data);
+  assert_return_code(eloop_register_timeout(test_state->eloop, 0, 1,
+                                            eloop_timeout_handler_function,
+                                            eloop_data, user_data),
+                     0);
+  assert_true(eloop_is_timeout_registered(test_state->eloop,
+                                          eloop_timeout_handler_function,
+                                          eloop_data, user_data));
+
+  // basic test
+  char eloop_data1[] = "this is eloop data: run 1";
+  char user_data1[] = "this is user data: run 1";
+  expect_string(eloop_timeout_handler_function, eloop_ctx, eloop_data1);
+  expect_string(eloop_timeout_handler_function, user_ctx, user_data1);
+  assert_return_code(eloop_register_timeout(test_state->eloop, 0, 100,
+                                            eloop_timeout_handler_function,
+                                            eloop_data1, user_data1),
+                     0);
+
+  // test cancelling timeouts
+  char eloop_data2[] = "this is eloop data: run 2";
+  char user_data2[] = "this is user data: run 2";
+  assert_return_code(eloop_register_timeout(test_state->eloop, 0, 200,
+                                            eloop_timeout_handler_function,
+                                            eloop_data2, user_data2),
+                     0);
+  assert_int_equal(
+      eloop_cancel_timeout(test_state->eloop, eloop_timeout_handler_function,
+                           eloop_data2, user_data2),
+      1 // should cancel only all = one timeout
+  );
+
+  assert_return_code(eloop_register_timeout(test_state->eloop, 1, 0,
+                                            eloop_timeout_handler_function,
+                                            eloop_data2, user_data2),
+                     0);
+  assert_return_code(eloop_register_timeout(test_state->eloop, 2, 0,
+                                            eloop_timeout_handler_function,
+                                            eloop_data2, user_data2),
+                     0);
+  assert_return_code(eloop_register_timeout(test_state->eloop, 3, 0,
+                                            eloop_timeout_handler_function,
+                                            eloop_data2, user_data2),
+                     0);
+
+  struct os_reltime remaining = {0};
+  assert_int_equal(eloop_cancel_timeout_one(
+                       test_state->eloop, eloop_timeout_handler_function,
+                       eloop_data2, user_data2, &remaining),
+                   1 // should cancel only one timeout
+  );
+  assert_int_equal(
+      eloop_cancel_timeout(test_state->eloop, eloop_timeout_handler_function,
+                           eloop_data2, user_data2),
+      2 // should cancel all = two timeouts
+  );
+
+  char eloop_data3[] = "this is eloop data: run 3";
+  char user_data3[] = "this is user data: run 3";
+  expect_string(eloop_timeout_handler_function, eloop_ctx, eloop_data3);
+  expect_string(eloop_timeout_handler_function, user_ctx, user_data3);
+  assert_return_code(eloop_register_timeout(test_state->eloop, 0, 300,
+                                            eloop_timeout_handler_function,
+                                            eloop_data3, user_data3),
+                     0);
+
+  // test depleting (shortening) timeouts
+  char eloop_data4[] = "this is eloop data: run 4";
+  expect_string(eloop_timeout_handler_function, eloop_ctx, eloop_data4);
+  expect_string(eloop_timeout_handler_function, user_ctx, user_data);
+  assert_return_code(eloop_register_timeout(test_state->eloop, 120,
+                                            0, // super long time
+                                            eloop_timeout_handler_function,
+                                            eloop_data4, user_data),
+                     0);
+  assert_int_equal(eloop_deplete_timeout(
+                       test_state->eloop, 100000, 0, // longer, so no change
+                       eloop_timeout_handler_function, eloop_data4, user_data),
+                   0);
+  assert_int_equal(eloop_deplete_timeout(test_state->eloop, 1, 0,
+                                         eloop_timeout_handler_function,
+                                         "this data does not exist", user_data),
+                   -1);
+  assert_int_equal(eloop_deplete_timeout(test_state->eloop, 0, 400,
+                                         eloop_timeout_handler_function,
+                                         eloop_data4, user_data),
+                   1);
+
+  // test replenishing (lengthening) timeouts
+  char eloop_data5[] = "this is eloop data: run 5";
+  expect_string(eloop_timeout_handler_function, eloop_ctx, eloop_data5);
+  expect_string(eloop_timeout_handler_function, user_ctx, user_data);
+  assert_return_code(eloop_register_timeout(test_state->eloop, 0,
+                                            2, // super short time
+                                            eloop_timeout_handler_function,
+                                            eloop_data5, user_data),
+                     0);
+  assert_int_equal(eloop_replenish_timeout(
+                       test_state->eloop, 0, 1, // no change, should return 0
+                       eloop_timeout_handler_function, eloop_data5, user_data),
+                   0);
+  assert_int_equal(eloop_replenish_timeout(test_state->eloop, 100000, 0,
+                                           eloop_timeout_handler_function,
+                                           "this data does not exist",
+                                           user_data),
+                   -1);
+  assert_int_equal(eloop_replenish_timeout(test_state->eloop, 0, 500,
+                                           eloop_timeout_handler_function,
+                                           eloop_data5, user_data),
+                   1);
+
+  log_debug("Starting eloop");
+  eloop_run(test_state->eloop);
+  log_debug("Finished eloop");
+}
+
 static void test_eloop_register_read_sock(void **state) {
   (void)state; /* unused */
   char *send_buf = TEST_SEND_BUF_DATA;
@@ -163,7 +320,8 @@ int main(int argc, char *argv[]) {
       cmocka_unit_test(test_eloop_register_read_sock),
       cmocka_unit_test(test_eloop_unregister_read_sock),
       cmocka_unit_test(test_eloop_register_timeout),
-      cmocka_unit_test(test_eloop_cancel_timeout)};
+      cmocka_unit_test(test_eloop_cancel_timeout),
+      cmocka_unit_test_setup_teardown(test_eloop_timeout, setup, teardown)};
 
   return cmocka_run_group_tests(tests, NULL, NULL);
 }
