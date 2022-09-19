bc85a49c219de200a00157e9dff18f64d8b9eb86
Alois Klink <alois@nquiringminds.com>
2022-09-19 09:14:11 +0100

    style: format tests/util/test_eloop.c using llvm
    Formats tests/util/test_eloop.c using the LLVM style guide.

--- tests/utils/test_eloop.c
+++ tests/utils/test_eloop.c
@@ -24,9 +24,7 @@ void test_eloop_sock_handler_read(int sock, void *eloop_ctx, void *sock_ctx) {
   assert_non_null(eloop);
   assert_string_equal(sock_ctx_data, TEST_ELOOP_PARAM);
 
-  struct client_address addr = {
-    .type = SOCKET_TYPE_DOMAIN
-  };
+  struct client_address addr = {.type = SOCKET_TYPE_DOMAIN};
   char read_buf[100];
   read_socket_data(sock, read_buf, sizeof(read_buf), &addr, 0);
   assert_string_equal(read_buf, TEST_SEND_BUF_DATA);
