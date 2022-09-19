nqminds/edgesec

commit 3c1e6da64d9e0d2dad383dbff737d0353f44321e

nl refactoring

src/utils/nl.c
@@ -37,11 +37,12 @@
 #include <linux/netlink.h>
 #include <arpa/inet.h>
 
-// #include <netlink/genl/genl.h>
-// #include <netlink/genl/family.h>
-// #include <netlink/genl/ctrl.h>
-// #include <netlink/msg.h>
-// #include <netlink/attr.h>
+
+#include <netlink/genl/genl.h>
+#include <netlink/genl/family.h>
+#include <netlink/genl/ctrl.h>
+#include <netlink/msg.h>
+#include <netlink/attr.h>
 
 #include "libnetlink.h"
 #include "ll_map.h"
@@ -49,7 +50,6 @@
 #include "rt_names.h"
 
 #include "linux/if_addr.h"
-// #include "linux/if_arp.h"
 #include "linux/if_infiniband.h"
 
 
@@ -58,6 +58,8 @@
 #include "os.h"
 #include "log.h"
 #include "nl.h"
+#include "net.h"
+#include "ifaceu.h"
 #include "utarray.h"
 
 static int ifindex = 0;
@@ -65,6 +67,23 @@ struct rtnl_handle rth = { .fd = -1 };
 static int have_rtnl_newlink = -1;
 
 static const UT_icd netif_info_icd = {sizeof(netif_info_t), NULL, NULL, NULL};
+static const UT_icd netiw_info_icd = {sizeof(netiw_info_t), NULL, NULL, NULL};
+
+static const char *ifmodes[NL80211_IFTYPE_MAX + 1] = {
+	"unspecified",
+	"IBSS",
+	"managed",
+	"AP",
+	"AP/VLAN",
+	"WDS",
+	"monitor",
+	"mesh point",
+	"P2P-client",
+	"P2P-GO",
+	"P2P-device",
+	"outside context of a BSS",
+	"NAN",
+};
 
 static int store_nlmsg(struct nlmsghdr *n, void *arg)
 {
@@ -823,4 +842,356 @@ bool nl_set_interface_state(char *if_name, bool state)
 err:
 	rtnl_close(&rth);
 	return false;
+}
+
+static void mac_addr_n2a(char *mac_addr, const unsigned char *arg)
+{
+	int i, l;
+
+	l = 0;
+	for (i = 0; i < ETH_ALEN ; i++) {
+		if (i == 0) {
+			sprintf(mac_addr+l, "%02x", arg[i]);
+			l += 2;
+		} else {
+			sprintf(mac_addr+l, ":%02x", arg[i]);
+			l += 3;
+		}
+	}
+}
+
+static const char *iftype_name(enum nl80211_iftype iftype, char *modebuf)
+{
+	if (iftype <= NL80211_IFTYPE_MAX && ifmodes[iftype])
+		return ifmodes[iftype];
+	sprintf(modebuf, "Unknown mode (%d)", iftype);
+	return modebuf;
+}
+
+static int error_handler(struct sockaddr_nl *nla, struct nlmsgerr *err,
+			 void *arg)
+{
+	(void) nla;
+	struct nlmsghdr *nlh = (struct nlmsghdr *)err - 1;
+	int len = nlh->nlmsg_len;
+	struct nlattr *attrs;
+	struct nlattr *tb[NLMSGERR_ATTR_MAX + 1];
+	int *ret = arg;
+	int ack_len = sizeof(*nlh) + sizeof(int) + sizeof(*nlh);
+
+	*ret = err->error;
+
+	if (!(nlh->nlmsg_flags & NLM_F_ACK_TLVS))
+		return NL_STOP;
+
+	if (!(nlh->nlmsg_flags & NLM_F_CAPPED))
+		ack_len += err->msg.nlmsg_len - sizeof(*nlh);
+
+	if (len <= ack_len)
+		return NL_STOP;
+
+	attrs = (void *)((unsigned char *)nlh + ack_len);
+	len -= ack_len;
+
+	nla_parse(tb, NLMSGERR_ATTR_MAX, attrs, len, NULL);
+	if (tb[NLMSGERR_ATTR_MSG]) {
+		len = strnlen((char *)nla_data(tb[NLMSGERR_ATTR_MSG]), nla_len(tb[NLMSGERR_ATTR_MSG]));
+		log_trace("kernel reports: %*s\n", len, (char *)nla_data(tb[NLMSGERR_ATTR_MSG]));
+	}
+
+	return NL_STOP;
+}
+
+static int finish_handler(struct nl_msg *msg, void *arg)
+{
+	(void) msg;
+	int *ret = arg;
+	*ret = 0;
+	return NL_SKIP;
+}
+
+static int ack_handler(struct nl_msg *msg, void *arg)
+{
+	(void) msg;
+	int *ret = arg;
+	*ret = 0;
+	return NL_STOP;
+}
+
+static int nl80211_init(struct nl80211_state *state)
+{
+	int err;
+
+	state->nl_sock = nl_socket_alloc();
+	if (!state->nl_sock) {
+		log_err("Failed to allocate netlink socket");
+		return -ENOMEM;
+	}
+
+	if (genl_connect(state->nl_sock)) {
+		log_err("Failed to connect to generic netlink");
+		err = -ENOLINK;
+		goto out_handle_destroy;
+	}
+
+	nl_socket_set_buffer_size(state->nl_sock, 8192, 8192);
+
+	/* try to set NETLINK_EXT_ACK to 1, ignoring errors */
+	err = 1;
+	setsockopt(nl_socket_get_fd(state->nl_sock), SOL_NETLINK,
+		   NETLINK_EXT_ACK, &err, sizeof(err));
+
+	state->nl80211_id = genl_ctrl_resolve(state->nl_sock, "nl80211");
+	if (state->nl80211_id < 0) {
+		log_err("nl80211 not found");
+		err = -ENOENT;
+		goto out_handle_destroy;
+	}
+
+	return 0;
+
+ out_handle_destroy:
+	nl_socket_free(state->nl_sock);
+	return err;
+}
+
+static int process_phy_handler(struct nl_msg *msg, void *arg)
+{
+	struct nlattr *tb_msg[NL80211_ATTR_MAX + 1];
+	struct genlmsghdr *gnlh = nlmsg_data(nlmsg_hdr(msg));
+
+	struct nlattr *nl_mode;
+	int rem_mode;
+	bool *isvalid = (bool*)arg;
+	char* capability;
+	char *wiphy;
+
+	nla_parse(tb_msg, NL80211_ATTR_MAX, genlmsg_attrdata(gnlh, 0), genlmsg_attrlen(gnlh, 0), NULL);
+
+	if (tb_msg[NL80211_ATTR_WIPHY_NAME])
+		wiphy = nla_get_string(tb_msg[NL80211_ATTR_WIPHY_NAME]);
+		log_debug("Using Wiphy %s", wiphy);
+
+	if (tb_msg[NL80211_ATTR_SUPPORTED_IFTYPES]) {
+		char modebuf[100];
+		nla_for_each_nested(nl_mode, tb_msg[NL80211_ATTR_SUPPORTED_IFTYPES], rem_mode) {
+			capability = (char *) iftype_name(nla_type(nl_mode), modebuf);
+			log_trace("%s -> %s", wiphy, capability);
+			if (!strcmp(capability, "AP/VLAN")) {
+				*isvalid = true;
+			}
+		}
+	}
+
+	return NL_SKIP;
+}
+
+static int process_iface_handler(struct nl_msg *msg, void *arg)
+{
+	UT_array *arr = (UT_array *) arg;
+	struct genlmsghdr *gnlh = nlmsg_data(nlmsg_hdr(msg));
+	struct nlattr *tb_msg[NL80211_ATTR_MAX + 1];
+	netiw_info_t element;
+
+	nla_parse(tb_msg, NL80211_ATTR_MAX, genlmsg_attrdata(gnlh, 0), genlmsg_attrlen(gnlh, 0), NULL);
+
+	if (tb_msg[NL80211_ATTR_IFNAME]) {
+		os_strlcpy(element.ifname, nla_get_string(tb_msg[NL80211_ATTR_IFNAME]), IFNAMSIZ);
+
+		if (tb_msg[NL80211_ATTR_IFINDEX]) {
+			element.ifindex = nla_get_u32(tb_msg[NL80211_ATTR_IFINDEX]);
+			log_trace("%s -> ifindex=%d", element.ifname, element.ifindex);
+		}
+
+		if (tb_msg[NL80211_ATTR_WDEV]) {
+			element.wdev = nla_get_u64(tb_msg[NL80211_ATTR_WDEV]);
+			log_trace("%s -> wdev=0x%llx", element.ifname, (unsigned long long)element.wdev);
+		}
+
+		if (tb_msg[NL80211_ATTR_MAC]) {
+			char mac_addr[20];
+			os_memcpy(element.addr, nla_data(tb_msg[NL80211_ATTR_MAC]), ETH_ALEN);
+			mac_addr_n2a(mac_addr, element.addr);
+			log_trace("%s -> addr=%s", element.ifname, mac_addr);
+		}
+
+		if (tb_msg[NL80211_ATTR_WIPHY]) {
+			element.wiphy = nla_get_u32(tb_msg[NL80211_ATTR_WIPHY]);
+			log_trace("%s -> wiphy=%d", element.ifname, element.wiphy);
+		}
+
+		utarray_push_back(arr, &element);
+	}
+
+	return NL_SKIP;
+}
+
+static int8_t nl_new(struct nl80211_state *nlstate, struct nl_cb **cb, struct nl_msg **msg, int *err)
+{
+	if (nl80211_init(nlstate)) {		
+		return -1;
+	}
+
+	*msg = nlmsg_alloc();
+	if (*msg == NULL) {
+		log_trace("failed to allocate netlink message");
+		nl_socket_free(nlstate->nl_sock);
+		return 1;
+	}
+
+	*cb = nl_cb_alloc(NL_CB_TYPE);
+	if (*cb ==  NULL) {
+		log_trace("failed to allocate netlink callbacks\n");
+		nlmsg_free(*msg);
+		nl_socket_free(nlstate->nl_sock);
+		return 1;
+	}
+
+	nl_cb_err(*cb, NL_CB_CUSTOM, error_handler, err);
+	nl_cb_set(*cb, NL_CB_FINISH, NL_CB_CUSTOM, finish_handler, err);
+	nl_cb_set(*cb, NL_CB_ACK, NL_CB_CUSTOM, ack_handler, err);
+
+	return 0;
+}
+
+bool iwace_isvlan(uint32_t wiphy)
+{
+	bool isvlan = false;
+	int err = 1;
+	struct nl_cb *cb;
+	struct nl_msg *msg;
+	struct nl80211_state nlstate;
+
+	if (nl_new(&nlstate, &cb, &msg, &err) != 0)
+		return false;
+
+	genlmsg_put(msg, 0, 0, nlstate.nl80211_id, 0, 0, NL80211_CMD_GET_WIPHY, 0);
+	NLA_PUT_U32(msg, NL80211_ATTR_WIPHY, wiphy);
+
+	if (nl_send_auto_complete(nlstate.nl_sock, msg) < 0) {
+		nl_cb_put(cb);
+		nlmsg_free(msg);
+		nl_socket_free(nlstate.nl_sock);
+		return false;
+	}
+
+	nl_cb_set(cb, NL_CB_VALID, NL_CB_CUSTOM, process_phy_handler, &isvlan);
+
+	while (err > 0) {
+		nl_recvmsgs(nlstate.nl_sock, cb);
+	}
+
+	nl_cb_put(cb);
+	nlmsg_free(msg);
+	nl_socket_free(nlstate.nl_sock);
+	return isvlan;
+
+ nla_put_failure:
+	log_trace("NLA_PUT_U32 failed");
+	nl_cb_put(cb);
+	nlmsg_free(msg);
+	nl_socket_free(nlstate.nl_sock);
+	return false;
+}
+
+UT_array *get_netiw_info(void)
+{
+	int err = 1;
+	struct nl80211_state nlstate;
+	struct nl_cb *cb;
+	struct nl_msg *msg;
+	UT_array *arr = NULL;
+	utarray_new(arr, &netiw_info_icd);
+
+	if (nl_new(&nlstate, &cb, &msg, &err) != 0)
+		return NULL;
+
+	genlmsg_put(msg, 0, 0, nlstate.nl80211_id, 0, NLM_F_DUMP, NL80211_CMD_GET_INTERFACE, 0);
+
+	if (nl_send_auto_complete(nlstate.nl_sock, msg) < 0) {
+		nl_cb_put(cb);
+		nlmsg_free(msg);
+		nl_socket_free(nlstate.nl_sock);
+		utarray_free(arr);
+		return NULL;
+	}
+
+	nl_cb_set(cb, NL_CB_VALID, NL_CB_CUSTOM, process_iface_handler, arr);
+
+	while (err > 0) {
+		nl_recvmsgs(nlstate.nl_sock, cb);
+	}
+
+	nl_cb_put(cb);
+	nlmsg_free(msg);
+	nl_socket_free(nlstate.nl_sock);
+	return arr;
+
+	log_trace("NLA_PUT_U32 failed");
+	nl_cb_put(cb);
+	nlmsg_free(msg);
+	nl_socket_free(nlstate.nl_sock);
+	utarray_free(arr);
+	return NULL;
+}
+
+int nl_is_iw_vlan(const char *ifname)
+{
+  log_debug("Checking %s exists", ifname);
+  if (!iface_exists(ifname)) {
+    log_trace("WiFi interface %s doesn't exist", ifname);
+    return 1;
+  }
+
+  UT_array *netif_list = get_netiw_info();
+
+  if (netif_list == NULL) {
+    log_trace("Couldn't list wifi interfaces");
+    return -1;
+  }
+
+  netiw_info_t *el;
+  for (el = (netiw_info_t*) utarray_front(netif_list); el != NULL; el = (netiw_info_t *) utarray_next(netif_list, el)) {
+    if (!strcmp(el->ifname, ifname)) {
+      if (!iwace_isvlan(el->wiphy)) {
+        log_trace("WiFi interface %s doesn't suport vlan tagging", ifname);
+        utarray_free(netif_list);
+        return 1;
+      } else {
+        utarray_free(netif_list);
+        return 0;
+      }
+    }
+  }
+
+  utarray_free(netif_list);
+  return -1;
+}
+
+char* nl_get_valid_iw(char *buf)
+{
+  UT_array *netif_list = get_netiw_info();
+
+  if (netif_list == NULL) {
+    log_trace("Couldn't list wifi interfaces");
+    return NULL;
+  }
+
+  if (buf == NULL) {
+    log_trace("if_buf param is NULL");
+    utarray_free(netif_list);
+    return NULL;
+  }
+
+  netiw_info_t *el;
+  for (el = (netiw_info_t*) utarray_front(netif_list); el != NULL; el = (netiw_info_t *) utarray_next(netif_list, el)) {
+    if (iwace_isvlan(el->wiphy)) {
+      os_strlcpy(buf, el->ifname, IFNAMSIZ);
+      utarray_free(netif_list);
+      return buf;
+    }
+  }
+
+  utarray_free(netif_list);
+  return NULL;
 }

CMakeLists.txt
@@ -1,7 +1,7 @@
 cmake_minimum_required(VERSION 3.11.0)
 # 3.11 required for FetchContent
 
-option(CROSS_COMPILE_EDGESEC "Cross compile the EDGESec" ON)
+option(CROSS_COMPILE_EDGESEC "Cross compile the EDGESec" OFF)
 
 if (CROSS_COMPILE_EDGESEC)
   set(CMAKE_SYSTEM_NAME Linux)
@@ -64,9 +64,9 @@ option(BUILD_SQLITE_LIB "Build sqlite3 library" ON)
 option(BUILD_OPENSSL_LIB "Build OpenSSL" ON)
 option(BUILD_CMOCKA_LIB "Build cmocka library" OFF)
 # off by default since we should apt install libmnl-dev
-option(BUILD_MNL_LIB "Build mnl library" OFF)
-option(BUILD_NETLINK_LIB "Build netlink library" OFF)
-option(BUILD_NL_LIB "Build nl library" OFF)
+option(BUILD_MNL_LIB "Build mnl library" ON)
+option(BUILD_NETLINK_LIB "Build netlink library" ON)
+option(BUILD_NL_LIB "Build nl library" ON)
 option(BUILD_MICROHTTPD_LIB "Build microhttpd library" OFF)
 option(BUILD_UUID_LIB "Build uuid library" ON)
 option(BUILD_PCAP_LIB "Build pcap library" ON)
@@ -75,7 +75,7 @@ option(BUILD_GRPC_LIB "Build gRPC library (warning, cross-compiling does not wor
 option(BUILD_SQLSYNC_SERVICE "Build sqlsync service" OFF)
 option(BUILD_REVERSE_SERVICE "Build Reverse service" OFF)
 
-option(USE_NETLINK_SERVICE "Use netlink service" OFF)
+option(USE_NETLINK_SERVICE "Use netlink service" ON)
 option(USE_GRPC_LIB "Use grpc library" OFF)
 option(USE_SQLITE3_LIB "Use sqlite3 library" ON)
 option(USE_UUID_LIB "Use uuid library" ON)

lib/mnl.cmake
@@ -1,5 +1,5 @@
 # Compile libmnl library used for libnetlink
-if (BUILD_MNL_LIB AND NOT (BUILD_ONLY_DOCS) AND USE_MNL_LIB)
+if (BUILD_MNL_LIB AND NOT (BUILD_ONLY_DOCS))
   set(LIBMNL_INSTALL_ROOT "${CMAKE_CURRENT_BINARY_DIR}/lib/mnl")
 
   # Tell find_package(MNL) to search in local built mnl dir
@@ -25,7 +25,7 @@ if (BUILD_MNL_LIB AND NOT (BUILD_ONLY_DOCS) AND USE_MNL_LIB)
     find_package(MNL REQUIRED)
   endif ()
   message("Found libmnl library: ${MNL_LIBRARIES}")
-elseif(NOT BUILD_ONLY_DOCS AND USE_MNL_LIB)
+elseif(NOT BUILD_ONLY_DOCS)
   # finding system install libmnl (e.g. installed via apt)
   find_package(MNL REQUIRED)
   message("Found libmnl library: ${MNL_LIBRARIES}")

lib/netlink.cmake
@@ -1,6 +1,6 @@
 # Compile library libnetlink
-if (USE_NETLINK_LIB)
-  add_compile_definitions(WITH_NETLINK_LIB)
+if (USE_NETLINK_SERVICE)
+  add_compile_definitions(WITH_NETLINK_SERVICE)
 endif ()
 
 if (BUILD_NETLINK_LIB AND NOT (BUILD_ONLY_DOCS))

src/CMakeLists.txt
@@ -14,7 +14,7 @@ add_subdirectory(capture)
 add_library(engine engine.c)
 if (BUILD_NL_LIB)
   target_include_directories(engine PRIVATE ${LIBNL_INCLUDE_DIR} ${LIBSQLITE_INCLUDE_DIR})
-  target_link_libraries(engine crypt_service log iface iw os sqlite_macconn_writer sqlite_fingerprint_writer subnet_service system_checks eloop supervisor network_commands radius_service mac_mapper ap_service iptables dhcp_service)
+  target_link_libraries(engine crypt_service log iface nl os sqlite_macconn_writer sqlite_fingerprint_writer subnet_service system_checks eloop supervisor network_commands radius_service mac_mapper ap_service iptables dhcp_service)
 else ()
   target_include_directories(engine PRIVATE ${LIBSQLITE_INCLUDE_DIR})
   target_link_libraries(engine crypt_service log iface os sqlite_macconn_writer sqlite_fingerprint_writer subnet_service system_checks eloop supervisor network_commands radius_service mac_mapper ap_service iptables dhcp_service)

src/engine.c
@@ -55,7 +55,6 @@
 #include "engine.h"
 #include "system/system_checks.h"
 #include "subnet/subnet_service.h"
-#include "utils/iw.h"
 #include "config.h"
 
 #define MACCONN_DB_NAME "macconn" SQLITE_EXTENSION
@@ -331,28 +330,19 @@ bool run_engine(struct app_config *app_config)
 
   log_info("Checking wifi interface...");
   if (!app_config->ap_detect) {
-#ifdef WITH_IW_SERVICE
-    ret = is_iw_vlan(context.hconfig.interface);
+    int ret = is_interface_vlan(context.hconfig.interface);
     if(ret > 0) {
       log_debug("interface %s not VLAN capable", context.hconfig.interface);
       goto run_engine_fail;
     } else if (ret < 0) {
       log_debug("is_iw_vlan fail");
     }
-#else
-  log_warn("iw service not implemented");
-#endif
   } else {
     log_info("Looking for VLAN capable wifi interface...");
-#ifdef WITH_IW_SERVICE
-    if(get_valid_iw(context.hconfig.interface) == NULL) {
+    if(get_vlan_interface(context.hconfig.interface) == NULL) {
       log_debug("get_valid_iw fail");
       goto run_engine_fail;
     }
-#else
-  log_warn("iw service not implemented");
-  goto run_engine_fail;
-#endif
   }
 
   log_info("Found wifi interface %s", context.hconfig.interface);

src/subnet/subnet_service.c
@@ -33,6 +33,7 @@
 #include "utils/log.h"
 #include "utils/allocs.h"
 #include "utils/os.h"
+#include "utils/net.h"
 #include "utils/iface.h"
 #include "utils/ifaceu.h"
 

src/utils/CMakeLists.txt
@@ -13,18 +13,15 @@ target_include_directories(os PRIVATE ${LIBUUID_INCLUDE_DIR})
 target_link_libraries(os PRIVATE allocs log ${LIBUUID_LIB})
 
 add_library(ifaceu ifaceu.c)
+target_link_libraries(ifaceu PRIVATE log)
 
 add_library(iptables iptables.c)
 target_link_libraries(iptables PRIVATE os log)
 
 if (USE_NETLINK_SERVICE)
   add_library(nl nl.c)
-  target_include_directories(nl PRIVATE ${LIBNETLINK_UAPI_INCLUDE_PATH} ${LIBNETLINK_INCLUDE_PATH})
-  target_link_libraries(nl PRIVATE ${LIBNL_LIBRARY} ${LIBNL_GENL_LIBRARY} ${LIBNETLINK_LIB} MNL::mnl ${LL_MAP_LIB} ${UTILS_LIB} ${RT_NAMES_LIB} log os)
-
-  add_library(iw iw.c)
-  target_include_directories(iw PRIVATE ${LIBNL_INCLUDE_DIR})
-  target_link_libraries(iw PRIVATE ${LIBNL_LIBRARY} ${LIBNL_GENL_LIBRARY} ${LIBNETLINK_LIB} MNL::mnl ${LL_MAP_LIB} ${UTILS_LIB} ${RT_NAMES_LIB} log)
+  target_include_directories(nl PRIVATE ${LIBNETLINK_UAPI_INCLUDE_PATH} ${LIBNETLINK_INCLUDE_PATH} ${LIBNL_INCLUDE_DIR})
+  target_link_libraries(nl PRIVATE ifaceu ${LIBNL_LIBRARY} ${LIBNL_GENL_LIBRARY} ${LIBNETLINK_LIB} MNL::mnl ${LL_MAP_LIB} ${UTILS_LIB} ${RT_NAMES_LIB} log os)
 endif ()
 
 add_library(iface iface.c)

commit a759205af67600a161242d141cc2d1464f1a69a0

fix(nl): fix missing curly brackets
According to the indentation, this was supposed to be part of the
original statement, but because there were no curly braces, the
auto-formatter reformatted it.

See https://github.com/nqminds/edgesec/blob/8cd691957f4b727f81d08a0e452caf22102de317/src/utils/nl.c#L1058-L1060

Fixes: 3c1e6da64d9e0d2dad383dbff737d0353f44321e

src/utils/nl.c
@@ -1028,9 +1028,10 @@ static int process_phy_handler(struct nl_msg *msg, void *arg) {
   nla_parse(tb_msg, NL80211_ATTR_MAX, genlmsg_attrdata(gnlh, 0),
             genlmsg_attrlen(gnlh, 0), NULL);
 
-  if (tb_msg[NL80211_ATTR_WIPHY_NAME])
+  if (tb_msg[NL80211_ATTR_WIPHY_NAME]) {
     wiphy = nla_get_string(tb_msg[NL80211_ATTR_WIPHY_NAME]);
-  log_trace("Using Wiphy %s", wiphy);
+    log_trace("Using Wiphy %s", wiphy);
+  }
 
   if (tb_msg[NL80211_ATTR_SUPPORTED_IFTYPES]) {
     char modebuf[100];