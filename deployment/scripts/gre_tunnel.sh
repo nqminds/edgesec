c8c1c40b13868fa90004b6f33455e7354e9f656d
Alexandru Mereacre <mereacre@gmail.com>
2022-09-26 10:51:14 +0100

    Update deployment/scripts/gre_tunnel.sh
    Co-authored-by: Alois Klink <alois@nquiringminds.com>

--- deployment/scripts/gre_tunnel.sh
+++ deployment/scripts/gre_tunnel.sh
@@ -3,11 +3,11 @@
 #ubuntu@host-2:~$ sudo ip addr add 172.17.0.2/24 dev tun0
 #ubuntu@host-2:~$ sudo ip link set tun0 up
 
-TAP_IFACE=$1
-TAP_IP=$2
-LOCAL_IP=$3
-REMOTE_IP=$4
-INTERFACE=$5
+TAP_IFACE="$1"
+TAP_IP="$2"
+LOCAL_IP="$3"
+REMOTE_IP="$4"
+INTERFACE="$5"
 
 if [ $# -lt 5 ]; then
     echo "Usage: ${0/*\//} <tap interface> <tap ip> <local ip> <remote ip> <interface>"
