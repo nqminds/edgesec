8114f7b114f4a79e3e1813804072a0c56d1a3c02
Alois Klink <alois@nquiringminds.com>
2022-09-27 11:40:42 +0100

    refactor(recap): rename recap to edgesec-recap
    There is already a Debian package called recap that has a binary
    called recap, so we should pick another name.

--- src/edgesec-recap.c
+++ src/edgesec-recap.c
@@ -73,7 +73,7 @@ struct pcap_stream_context {
 };
 
 void show_app_version(void) {
-  fprintf(stdout, "recap app version %s\n", EDGESEC_VERSION);
+  fprintf(stdout, "edgesec-recap app version %s\n", EDGESEC_VERSION);
 }
 
 void show_app_help(char *app_name) {
