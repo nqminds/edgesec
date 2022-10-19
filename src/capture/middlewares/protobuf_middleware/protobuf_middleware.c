3e6b9ff4222bddf8f0917d6b1870ff4e89ceefad
Alexandru Mereacre <mereacre@gmail.com>
2022-10-19 16:23:43 +0100

    Added the proto generated files

--- src/capture/middlewares/protobuf_middleware/protobuf_middleware.c
+++ src/capture/middlewares/protobuf_middleware/protobuf_middleware.c
@@ -26,6 +26,18 @@
 
 #include "../../pcap_service.h"
 
+#include "eth.pb-c.h"
+#include "arp.pb-c.h"
+#include "ip4.pb-c.h"
+#include "ip6.pb-c.h"
+#include "tcp.pb-c.h"
+#include "udp.pb-c.h"
+#include "icmp4.pb-c.h"
+#include "icmp6.pb-c.h"
+#include "dns.pb-c.h"
+#include "mdns.pb-c.h"
+#include "dhcp.pb-c.h"
+
 void free_protobuf_middleware(struct middleware_context *context) {
   (void) context;
 }
