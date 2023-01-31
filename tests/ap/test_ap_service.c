22c4a86836113ae17686806319bee3fc114c9361
Alois Klink <alois@nquiringminds.com>
2023-01-31 10:04:55 +0000

    test(ap_service): fix return for generate_*_conf()
    The generate_*_conf() functions previously returned a `bool`,
    with `true` for success and `false` for failure.
    
    However, 3196584b (Added hostapd uci integration, 2022-03-21)
    changed this behaviour, and made those functions use a return code,
    where `0` is success, and `-1` is for failures.
    
    Fixes: 3196584bfea28817259dba98f4853609f5051f8a

--- tests/ap/test_ap_service.c
+++ tests/ap/test_ap_service.c
@@ -25,11 +25,11 @@
 #include "utils/log.h"
 #include "utils/os.h"
 
-bool __wrap_generate_vlan_conf(char *vlan_file, char *interface) {
+int __wrap_generate_vlan_conf(char *vlan_file, char *interface) {
   (void)vlan_file;
   (void)interface;
 
-  return true;
+  return 0;
 }
 
 int __wrap_run_ap_process(struct apconf *hconf) {
@@ -38,12 +38,12 @@ int __wrap_run_ap_process(struct apconf *hconf) {
   return 0;
 }
 
-bool __wrap_generate_hostapd_conf(struct apconf *hconf,
-                                  struct radius_conf *rconf) {
+int __wrap_generate_hostapd_conf(struct apconf *hconf,
+                                 struct radius_conf *rconf) {
   (void)hconf;
   (void)rconf;
 
-  return true;
+  return 0;
 }
 
 int __wrap_signal_ap_process(struct apconf *hconf) {
