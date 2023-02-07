7d7a38d3792714dec08b2ee2af766c9b78e42888
Alois Klink <alois@nquiringminds.com>
2023-02-07 15:47:53 +0000

    refactor(md5): inline hmac_md5_vector in md5.c
    We never use the hmac_md5_vector() function anywhere except in md5.c,
    so we might as well inline it. This also helps avoid linker conflicts
    when linking libeap, which also has a hmac_md5_vector.

--- src/radius/md5.h
+++ src/radius/md5.h
@@ -18,8 +18,6 @@
 
 #define MD5_MAC_LEN 16
 
-int hmac_md5_vector(const uint8_t *key, size_t key_len, size_t num_elem,
-                    const uint8_t *addr[], const size_t *len, uint8_t *mac);
 int hmac_md5(const uint8_t *key, size_t key_len, const uint8_t *data,
              size_t data_len, uint8_t *mac);
 
