b8123951fc2bf953ceb4e1016e11554ea5524a50
Alois Klink <alois@nquiringminds.com>
2023-02-07 13:58:59 +0000

    refactor(os): rename os_get_random with `edge_`
    Rename `os_get_random()` to `edge_os_get_random()` to avoid linking
    conflicts with libeap.
    
    I've added a `#define os_get_random edge_os_get_random` macro so that
    we don't need to change references in the code.

--- src/utils/os.h
+++ src/utils/os.h
@@ -151,6 +151,8 @@ int os_get_timestamp(uint64_t *timestamp);
  */
 void os_to_timestamp(struct timeval ts, uint64_t *timestamp);
 
+#define os_get_random(buf, len) edge_os_get_random((buf), (len))
+
 /**
  * @brief Get cryptographically strong pseudo random data
  *
@@ -158,7 +160,7 @@ void os_to_timestamp(struct timeval ts, uint64_t *timestamp);
  * @param len Length of the buffer.
  * @return int 0 on success, -1 on failure
  */
-int os_get_random(unsigned char *buf, size_t len);
+int edge_os_get_random(unsigned char *buf, size_t len);
 
 /**
  * @brief Return a random int from a give range
