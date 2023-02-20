5f03e137ea84d9752a89c60784c66c30b0da0fea
Alois Klink <alois@nquiringminds.com>
2023-02-20 18:08:27 +0000

    refactor: add header file for compiler attributes
    Add `./src/utils/attributes.h` header file for compiler attributes.
    
    For example, `__attribute__((unused))` is supported on GCC and Clang,
    but on other compilers, this is not supported.
    
    Most of these are already in `src/radius/common.h`, so we just need to
    move them into a new file to use them in the rest of the edgesec code.

---
+++ src/utils/attributes.h
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * @author Alois Klink <alois@nquiringminds.com>
+ * @date 2023
+ * @copyright
+ * SPDX-FileCopyrightText: © 2023 NQMCyber Ltd and edgesec contributors
+ * SPDX-License-Identifier: Expat
+ * @brief File containing macros for compiler attributes, if they are supported.
+ *
+ * In the future, once we support C23, we can remove this header and just
+ * use C23 attributes.
+ */
+#ifndef ATTRIBUTES_H
+#define ATTRIBUTES_H
+
+#endif /* ATTRIBUTES_H */
