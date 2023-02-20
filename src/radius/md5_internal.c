7e995ea4b4df412ca9e1b8435d317b604d7c9f70
Alois Klink <alois@nquiringminds.com>
2023-02-20 13:02:32 +0000

    refactor(md5_internal): link MD5Init internally
    Remove `MD5Init()` from `md5_internal.h` and use `static` to declare
    it with internal linkage.
    
    We never call `MD5Init()` from outside of `md5_internal.c`, and the
    current name causes linker conflicts with libeap.

--- src/radius/md5_internal.c
+++ src/radius/md5_internal.c
@@ -18,6 +18,7 @@
 
 #include "md5_internal.h"
 
+static void MD5Init(struct MD5Context *ctx);
 static void MD5Transform(uint32_t buf[4], uint32_t const in[16]);
 
 typedef struct MD5Context MD5_CTX;
@@ -73,7 +74,7 @@ static void byteReverse(unsigned char *buf, unsigned longs) {
  * Start MD5 accumulation.  Set bit count to 0 and buffer to mysterious
  * initialization constants.
  */
-void MD5Init(struct MD5Context *ctx) {
+static void MD5Init(struct MD5Context *ctx) {
   ctx->buf[0] = 0x67452301;
   ctx->buf[1] = 0xefcdab89;
   ctx->buf[2] = 0x98badcfe;
