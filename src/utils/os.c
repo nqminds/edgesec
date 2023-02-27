nqminds/edgesec

commit f25fcb6e0fa3da246dceae28ceae63052eb57593

tests and pass store

src/utils/os.c
@@ -35,6 +35,11 @@
 
 #define MAX_64BIT_DIGITS 19
 
+struct proc_signal_arg {
+  char *proc_name;
+  int sig;
+};
+
 bool is_number(const char *ptr)
 {
   if (ptr == NULL)
@@ -671,16 +676,55 @@ bool kill_dir_fn(char *path, void *args)
   return true;
 }
 
-bool kill_process(char *proc_name)
+bool signal_dir_fn(char *path, void *args)
 {
-  // Kill a process process if running
-  log_trace("Killing process %s", proc_name);
-  if (list_dir("/proc", kill_dir_fn, proc_name) == -1) {
+  struct proc_signal_arg *sarg = (struct proc_signal_arg *) args;
+
+  unsigned long pid;
+  pid_t current_pid = getpid();
+  pid_t current_pid_group = getpgid(current_pid);
+
+  if ((pid = is_proc_app(path, sarg->proc_name)) != 0) {
+    if (current_pid != pid && pid != current_pid_group) {
+      log_trace("Found process pid=%d current_pid=%d current_pid_group=%d", pid, current_pid, current_pid_group);
+      if (kill(pid, sarg->sig) == -1) {
+        log_err("kill");
+        return false;
+      } else
+        log_trace("signalled %s process with pid=%d and sig=%d", sarg->proc_name, pid, sarg->sig);
+    }
+  }
+
+  return true;
+}
+
+bool signal_process(char *proc_name, int sig)
+{
+  struct proc_signal_arg sarg = {.proc_name = proc_name, .sig = sig};
+
+  if (proc_name == NULL) {
+    log_trace("proc_name is NULL");
+    return false;
+  }
+
+  if (!os_strnlen_s(proc_name, MAX_OS_PATH_LEN)) {
+    log_trace("proc_name is empty");
+    return false;
+  }
+
+  // Signal a process process if running
+  log_trace("Signalling process %s with signal=%d", proc_name, sig);
+  if (list_dir("/proc", signal_dir_fn, &sarg) == -1) {
     log_trace("list_dir fail");
     return false;
   }
 
   return true;
+}
+
+bool kill_process(char *proc_name)
+{
+  return signal_process(proc_name, SIGTERM);
 }
 
 char* string_array2string(char *strings[])

src/ap/ap_config.h
@@ -71,7 +71,7 @@ struct apconf {
   int logger_syslog_level;                            /**< The hostapd @c logger_syslog_level param @see https://w1.fi/cgit/hostap/plain/hostapd/hostapd.conf */
   int ignore_broadcast_ssid;                          /**< The hostapd @c ignore_broadcast_ssid param @see https://w1.fi/cgit/hostap/plain/hostapd/hostapd.conf */
   int wpa_psk_radius;                                 /**< The hostapd @c wpa_psk_radius param @see https://w1.fi/cgit/hostap/plain/hostapd/hostapd.conf */
-  char vlan_tagged_interface[MAX_OS_PATH_LEN];        /**< The hostapd @c vlan_tagged_interface param @see https://w1.fi/cgit/hostap/plain/hostapd/hostapd.conf */
+  char vlan_tagged_interface[IFNAMSIZ];        /**< The hostapd @c vlan_tagged_interface param @see https://w1.fi/cgit/hostap/plain/hostapd/hostapd.conf */
 };
 
 #endif

src/ap/ap_service.c
@@ -31,20 +31,24 @@
 #include "utils/if.h"
 #include "utils/log.h"
 
-char* run_ap(struct apconf *hconf, struct radius_conf *rconf, char *ctrl_if_path)
+int run_ap(struct apconf *hconf, struct radius_conf *rconf, char *ctrl_if_path, bool exec_ap)
 {
   if (!generate_vlan_conf(hconf->vlan_file, hconf->interface)) {
     log_trace("generate_vlan_conf fail");
-    return NULL;
+    return -1;
   }
 
   if (!generate_hostapd_conf(hconf, rconf)) {
     unlink(hconf->vlan_file);
     log_trace("generate_hostapd_conf fail");
-    return NULL;
+    return -1;
   }
 
-  return run_ap_process(hconf, ctrl_if_path);
+  if (exec_ap) {
+    return run_ap_process(hconf, ctrl_if_path);
+  } else {
+    return signal_ap_process(hconf->ap_bin_path);
+  }
 }
 
 bool close_ap(void)

src/ap/ap_service.h
@@ -41,9 +41,10 @@
  * @param hconf The AP configuration structure
  * @param rconf The radius configuration structure
  * @param ctrl_if_path The path of the AP control interface
- * @return char* The pointer to the statically allocated process name, NULL on failure
+ * @param exec_ap Flag to execute the AP process
+ * @return int 0 on success, -1 on failure
  */
-char* run_ap(struct apconf *hconf, struct radius_conf *rconf, char *ctrl_if_path);
+int run_ap(struct apconf *hconf, struct radius_conf *rconf, char *ctrl_if_path, bool exec_ap);
 
 /**
  * @brief Closes (terminates) AP process

src/ap/hostapd.c
@@ -154,7 +154,7 @@ int check_ctrl_if_exists(char *ctrl_if_path)
   return 0;
 }
 
-char* run_ap_process(struct apconf *hconf, char *ctrl_if_path)
+int run_ap_process(struct apconf *hconf, char *ctrl_if_path)
 {
   pid_t child_pid;
   int ret, check_count = 0;
@@ -168,13 +168,13 @@ char* run_ap_process(struct apconf *hconf, char *ctrl_if_path)
   // Kill any running hostapd process
   if (!kill_process(hostapd_proc_name)) {
     log_trace("kill_process fail");
-    return NULL;
+    return -1;
   }
 
   log_trace("Resetting wifi interface %s", hconf->interface);
   if (!reset_interface(hconf->interface)) {
     log_debug("reset_interface fail");
-    return NULL;
+    return -1;
   }
 
   while((ret = run_process(process_argv, &child_pid)) < 0) {
@@ -182,7 +182,7 @@ char* run_ap_process(struct apconf *hconf, char *ctrl_if_path)
     // Kill any running hostapd process
     if (!kill_process(hostapd_proc_name)) {
       log_trace("kill_process fail");
-      return NULL;
+      return -1;
     }
     log_trace("Restarting process in %d seconds", PROCESS_RESTART_TIME);
     sleep(PROCESS_RESTART_TIME);
@@ -190,12 +190,12 @@ char* run_ap_process(struct apconf *hconf, char *ctrl_if_path)
 
   if (ret > 0) {
     log_trace("hostapd process exited with status=%d", ret);
-    return NULL;
+    return -1;
   }
 
   if (list_dir("/proc", find_dir_proc_fn, (void *)&dir_args) == -1) {
     log_trace("list_dir fail");
-    return NULL;
+    return -1;
   }
 
   if (!dir_args.proc_running) {
@@ -216,11 +216,11 @@ char* run_ap_process(struct apconf *hconf, char *ctrl_if_path)
 
   if (check_count >= 7) {
     log_trace("hostapd unix domain control path fail");
-    return NULL;
+    return -1;
   }
 
   ap_process_started = true;
-  return hostapd_proc_name;
+  return 0;
 }
 
 bool kill_ap_process(void)
@@ -232,4 +232,17 @@ bool kill_ap_process(void)
   }
 
   return true;
+}
+
+int signal_ap_process(char *ap_bin_path)
+{
+  os_strlcpy(hostapd_proc_name, basename(ap_bin_path), MAX_OS_PATH_LEN);
+
+  // Signal any running hostapd process to reload the config
+  if (!signal_process(hostapd_proc_name, SIGHUP)) {
+    log_trace("signal_process fail");
+    return -1;
+  }
+
+  return 0;
 }

src/ap/hostapd.h
@@ -57,9 +57,17 @@ bool generate_vlan_conf(char *vlan_file, char *interface);
  * 
  * @param hconf The hostapd process config structure
  * @param ctrl_if_path The hostpad control interface
- * @return char* The pointer to the statically allocated process name, NULL on failure
+ * @return int 0 on success, -1 on failure
  */
-char* run_ap_process(struct apconf *hconf, char *ctrl_if_path);
+int run_ap_process(struct apconf *hconf, char *ctrl_if_path);
+
+/**
+ * @brief Signal the AP process to reload the config
+ * 
+ * @param ap_bin_path The AP process binary path
+ * @return int 0 on success, -1 on failure
+ */
+int signal_ap_process(char *ap_bin_path);
 
 /**
  * @brief Terminate the AP service

src/capture/capture_config.h
@@ -48,7 +48,7 @@
 struct capture_conf {
   char capture_bin_path[MAX_OS_PATH_LEN];                     /**< The capture binary path string */
   char domain_server_path[MAX_OS_PATH_LEN];                   /**< Specifies the path to the UNIX domain socket server */
-  char domain_command[64];                                    /**< Specifies the UNIX domain command */
+  char domain_command[MAX_SUPERVISOR_CMD_SIZE];                                    /**< Specifies the UNIX domain command */
   char domain_delim;                                          /**< Specifies the UNIX domain command delimiter */
   char capture_interface[IFNAMSIZ];                           /**< The capture interface name (any - to capture on all interfaces) */
   bool promiscuous;                                           /**< Specifies whether the interface is to be put into promiscuous mode. If promiscuous param is non-zero, promiscuous mode will be set, otherwise it will not be set */

src/capture/capture_service.c
@@ -50,8 +50,8 @@ int capture_opt2config(char key, char *value, struct capture_conf *config)
       os_strlcpy(config->capture_interface, value, IFNAMSIZ);
       break;
     case 'f':
-      config->filter = os_malloc(strlen(value) + 1);
-      strcpy(config->filter, value);
+      config->filter = os_malloc(MAX_FILTER_SIZE);
+      os_strlcpy(config->filter, value, MAX_FILTER_SIZE);
       break;
     case 'm':
       config->promiscuous = true;
@@ -90,7 +90,7 @@ int capture_opt2config(char key, char *value, struct capture_conf *config)
       os_strlcpy(config->domain_server_path, value, MAX_OS_PATH_LEN);
       break;
     case 'x':
-      os_strlcpy(config->domain_command, value, 64 - 1);
+      os_strlcpy(config->domain_command, value, MAX_SUPERVISOR_CMD_SIZE);
       break;
     case 'z':
       errno = 0;
@@ -135,7 +135,7 @@ char** capture_config2opt(struct capture_conf *config)
   }
 
   // capture_interface, -i
-  if (strlen(config->capture_interface)) {
+  if (os_strnlen_s(config->capture_interface, IFNAMSIZ)) {
     opt_str[idx] = os_zalloc(3);
     strcpy(opt_str[idx], "-i");
     idx ++;
@@ -146,7 +146,7 @@ char** capture_config2opt(struct capture_conf *config)
   }
 
   // filter, -f
-  if (strlen(config->filter)) {
+  if (os_strnlen_s(config->filter, MAX_FILTER_SIZE)) {
     opt_str[idx] = os_zalloc(3);
     strcpy(opt_str[idx], "-f");
     idx ++;
@@ -184,7 +184,7 @@ char** capture_config2opt(struct capture_conf *config)
   idx ++;
 
   // analyser, -y
-  if (strlen(config->analyser)) {
+  if (os_strnlen_s(config->analyser, MAX_ANALYSER_NAME_SIZE)) {
     opt_str[idx] = os_zalloc(3);
     strcpy(opt_str[idx], "-y");
     idx ++;
@@ -221,7 +221,7 @@ char** capture_config2opt(struct capture_conf *config)
   }
 
   //domain_server_path, -q
-  if (strlen(config->domain_server_path)) {
+  if (os_strnlen_s(config->domain_server_path, MAX_OS_PATH_LEN)) {
     opt_str[idx] = os_zalloc(3);
     strcpy(opt_str[idx], "-q");
     idx ++;
@@ -232,13 +232,13 @@ char** capture_config2opt(struct capture_conf *config)
   }
 
   //domain_command, -x
-  if (strlen(config->domain_command)) {
+  if (os_strnlen_s(config->domain_command, MAX_SUPERVISOR_CMD_SIZE)) {
     opt_str[idx] = os_zalloc(3);
     strcpy(opt_str[idx], "-x");
     idx ++;
 
-    opt_str[idx] = os_zalloc(strlen(config->domain_command) + 1);
-    strcpy(opt_str[idx], config->domain_command);
+    opt_str[idx] = os_malloc(MAX_SUPERVISOR_CMD_SIZE);
+    os_strlcpy(opt_str[idx], config->domain_command, MAX_SUPERVISOR_CMD_SIZE);
     idx ++;
   }
 
@@ -255,7 +255,7 @@ char** capture_config2opt(struct capture_conf *config)
   }
 
   //db_path, -p
-  if (strlen(config->db_path)) {
+  if (os_strnlen_s(config->db_path, MAX_OS_PATH_LEN)) {
     opt_str[idx] = os_zalloc(3);
     strcpy(opt_str[idx], "-p");
     idx ++;

commit c0122002fc6e8fb007f8f1cdaedd2509d2baa0e2

refactor(os): remove unused `kill_dir_fn()`
The `kill_dir_fn()` is not used anywhere in edgesec. The
more generic `signal_dir_fn()` fully replaced it in
f25fcb6e (tests and pass store, 2021-06-23).

Fixes: f25fcb6e0fa3da246dceae28ceae63052eb57593

src/utils/os.c
@@ -851,25 +851,6 @@ long is_proc_app(char *path, char *proc_name) {
   return 0;
 }
 
-bool kill_dir_fn(char *path, void *args) {
-  pid_t pid;
-  pid_t current_pid = getpid();
-  pid_t current_pid_group = getpgid(current_pid);
-  if ((pid = is_proc_app(path, args)) != 0) {
-    if (current_pid != pid && pid != current_pid_group) {
-      log_trace("Found process pid=%d current_pid=%d current_pid_group=%d", pid,
-                current_pid, current_pid_group);
-      if (kill(pid, SIGTERM) == -1) {
-        log_errno("kill");
-        return false;
-      } else
-        log_trace("killed %s process with pid=%d", args, pid);
-    }
-  }
-
-  return true;
-}
-
 bool signal_dir_fn(char *path, void *args) {
   struct proc_signal_arg *sarg = (struct proc_signal_arg *)args;
 