ce7bf4724cc15f45033bfc88c43213b8e4afaf96
Alois Klink <alois@nquiringminds.com>
2023-03-06 15:11:16 +0000

    fix(uci_wrt): fix broken uwrt_lookup_package()
    uwrt_lookup_package() is currently never adding anything to `list()`,
    which means that the count for anonymous section references is
    always 0. (it also means that we have a memory leak).
    
    Instead of building our own linked-list structure to make a counter,
    we should use a map/dictionary instead. I've implemented a basic
    one using `uthash.h`.
    
    Manually tested by adding `log_trace()` commands.
    
    Fixes: https://github.com/nqminds/edgesec/issues/481

--- src/utils/uci_wrt.c
+++ src/utils/uci_wrt.c
@@ -32,10 +32,19 @@
 
 #define IP_SECTION_STR "%d%d%d%d"
 
-struct uci_type_list {
+#include <uthash.h>
+
+/**
+ * @brief Hashmap that stores the count of all the seen section types.
+ *
+ * Use uci_clear_section_types_count() to clear this hashmap.
+ */
+struct uci_section_type_count {
+  // the type of this section (stored on the heap)
+  char *type;
+  // how many times we've encounted this type already
   unsigned int idx;
-  const char *name;
-  struct uci_type_list *next;
+  UT_hash_handle hh; /* makes this structure hashable */
 };
 
 static const UT_icd netif_info_icd = {sizeof(netif_info_t), NULL, NULL, NULL};
@@ -53,13 +62,21 @@ void uwrt_print_error(struct uci_context *ctx, const char *name) {
   }
 }
 
-void uci_reset_typelist(struct uci_type_list *list) {
-  struct uci_type_list *type = NULL;
+/**
+ * @brief Clears the given section type count hashmap.
+ *
+ * @param[in,out] section_types_count The head of the hashmap to clear. Will
+ * be set to `NULL` when done.
+ */
+static void uci_clear_section_types_count(
+    struct uci_section_type_count **section_types_count) {
+  struct uci_section_type_count *current_type_count, *tmp;
 
-  while (list != NULL) {
-    type = list;
-    list = list->next;
-    os_free(type);
+  HASH_ITER(hh, *section_types_count, current_type_count, tmp) {
+    HASH_DEL(*section_types_count,
+             current_type_count); /* delete entry from hashmap */
+    free(current_type_count->type);
+    free(current_type_count);
   }
 }
 
@@ -72,8 +89,9 @@ void uci_reset_typelist(struct uci_type_list *list) {
  * in `/etc/config/system`.
  *
  * @param s - The UCI section to find the reference to.
- * @param list - Section type list. Should contain all the previous section
- * entries.
+ * @param[in, out] list - UCI section type count hashmap.
+ * Should contain the count of all the previous section types, and will be
+ * updated to include the current section type.
  * @param[in, out] typestr - malloc()-ed buffer that can be realloc()-ed
  * as required, or `NULL`. Please `free()` this parameter after this function
  * has been called.
@@ -83,28 +101,28 @@ void uci_reset_typelist(struct uci_type_list *list) {
  * be invalid once either of them are `free()`-ed.
  */
 static char *uci_lookup_section_ref(struct uci_section *s,
-                                    struct uci_type_list *list,
+                                    struct uci_section_type_count **section_types_count,
                                     char **typestr) {
 
-  /* look up in section type list */
-  struct uci_type_list *ti = list;
-  while (ti != NULL) {
-    if (strcmp(ti->name, s->type) == 0) {
-      break;
-    }
-
-    ti = ti->next;
-  }
-
-  if (ti == NULL) {
-    if ((ti = os_calloc(1, sizeof(struct uci_type_list))) == NULL) {
-      log_errno("os_calloc");
+  struct uci_section_type_count *section_type_count;
+  HASH_FIND_STR(*section_types_count, s->type, section_type_count);
+  if (section_type_count == NULL) {
+    section_type_count = os_malloc(sizeof(struct uci_section_type_count));
+    if (section_type_count == NULL) {
+      log_errno("os_malloc");
       return NULL;
     }
-
-    ti->next = list;
-    list = ti;
-    ti->name = s->type;
+    *section_type_count = (struct uci_section_type_count){
+        .idx = 0,
+        .type = os_strdup(s->type),
+    };
+    if (section_type_count->type == NULL) {
+      log_errno("os_strdup");
+      free(section_type_count);
+      return NULL;
+    }
+    HASH_ADD_KEYPTR(hh, *section_types_count, section_type_count->type,
+                    strlen(section_type_count->type), section_type_count);
   }
 
   char *ret;
@@ -122,14 +140,14 @@ static char *uci_lookup_section_ref(struct uci_section *s,
       *typestr = p;
     }
 
-    sprintf(*typestr, "@%s[%d]", ti->name, ti->idx);
+    sprintf(*typestr, "@%s[%d]", s->type, section_type_count->idx);
 
     ret = *typestr;
   } else {
     ret = s->e.name;
   }
 
-  ti->idx++;
+  section_type_count->idx++;
 
   return ret;
 }
@@ -230,14 +248,17 @@ int uwrt_lookup_section(struct uci_section *s, char *sref, UT_array *kv) {
 
 int uwrt_lookup_package(struct uci_package *p, UT_array *kv) {
   struct uci_element *e = NULL;
-  struct uci_type_list *list = NULL;
+  /** Counts the section types we've already encountered */
+  struct uci_section_type_count *section_types_count = NULL;
+
   char *typestr = NULL;
   char *sref = NULL;
   int ret = 0;
 
   uci_foreach_element(&p->sections, e) {
     struct uci_section *s = uci_to_section(e);
-    if ((sref = uci_lookup_section_ref(s, list, &typestr)) == NULL) {
+    if ((sref = uci_lookup_section_ref(s, &section_types_count, &typestr)) ==
+        NULL) {
       log_trace("uci_lookup_section_ref fail");
       ret = -1;
       goto uwrt_lookup_package_fail;
@@ -250,7 +271,7 @@ int uwrt_lookup_package(struct uci_package *p, UT_array *kv) {
   }
 
 uwrt_lookup_package_fail:
-  uci_reset_typelist(list);
+  uci_clear_section_types_count(&section_types_count);
 
   if (typestr != NULL) {
     os_free(typestr);
