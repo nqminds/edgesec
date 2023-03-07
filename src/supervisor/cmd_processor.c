nqminds/edgesec

commit b04ad88be46b67a335fd2f78ad34eec6e75a373f

fix(cmd_processor): handle ENOMEM error in process_get_bridges_cmd()
Handle ENOMEM errors when allocating memory on the heap in
the function `process_get_bridges_cmd()`.

This is required to avoid memory leaks and accessing NULL pointers
(which is undefined behaviour).

src/supervisor/cmd_processor.c
@@ -508,10 +508,20 @@ ssize_t process_get_bridges_cmd(int sock, struct client_address *client_addr,
       int line_size = snprintf(temp, 255, MACSTR "," MACSTR "\n",
                                MAC2STR(p->src_addr), MAC2STR(p->dst_addr));
       total += line_size + 1;
-      if (reply_buf == NULL)
-        reply_buf = os_zalloc(total);
-      else
-        reply_buf = os_realloc(reply_buf, total);
+      {
+        char *reallocated_reply_buf = os_realloc(reply_buf, total);
+        if (reallocated_reply_buf == NULL) {
+          log_errno("realloc: failed to allocate %s bytes", total);
+          free(reply_buf);
+          utarray_free(tuple_list_arr);
+          return -1;
+        }
+        if (reply_buf == NULL) {
+          // initialise the buffer as an empty string
+          reallocated_reply_buf[0] = '\0';
+        }
+        reply_buf = reallocated_reply_buf;
+      }
       strcat(reply_buf, temp);
     }
 

commit 186629cbfeeb51fb8b7ca1ee2d96deb74681548d

fix(cmd_processor): fix incorrect format specifier
We're using `%s` to format an `int`, which is incorrect.

Fixes: b04ad88be46b67a335fd2f78ad34eec6e75a373f

src/supervisor/cmd_processor.c
@@ -522,7 +522,7 @@ ssize_t process_get_bridges_cmd(int sock,
     {
       char *reallocated_reply_buf = os_realloc(reply_buf, total);
       if (reallocated_reply_buf == NULL) {
-        log_errno("realloc: failed to allocate %s bytes", total);
+        log_errno("realloc: failed to allocate %d bytes", total);
         free(reply_buf);
         utarray_free(tuple_list_arr);
         return -1;