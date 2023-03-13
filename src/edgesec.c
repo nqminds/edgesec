nqminds/edgesec

commit f74551353f899d8f31df1f8d9f16694b750a853f

refactor

src/edgesec.c
@@ -241,14 +241,6 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
-  /* Kill all edgesec processes if running */
-  if (config.kill_running_proc) {
-    if (!kill_process(get_app_name(argv[0]))) {
-      fprintf(stderr, "kill_process fail.\n");
-      return EXIT_FAILURE;
-    }
-  }
-
   if (create_pid_file(config.pid_file_path, FD_CLOEXEC) < 0) {
     fprintf(stderr, "create_pid_file fail");
     return EXIT_FAILURE;

config.ini.in
@@ -12,7 +12,6 @@ generateSsid = true
 allocateVlans = true
 defaultOpenVlanId = 0
 quarantineVlanId = 10
-killRunningProcess = false
 # Set to `true` to let edgesec process start hostapd
 execAp = true
 execRadius = true

deployment/owrt-config/config-dev.ini
@@ -11,7 +11,6 @@ generateSsid = true
 allocateVlans = true
 defaultOpenVlanId = 0
 quarantineVlanId = 10
-killRunningProcess = false
 execAp = true
 execRadius = true
 execDhcp = true

deployment/owrt-config/config.ini
@@ -11,7 +11,6 @@ generateSsid = true
 allocateVlans = true
 defaultOpenVlanId = 0
 quarantineVlanId = 10
-killRunningProcess = false
 execAp = true
 execRadius = true
 execDhcp = true

deployment/rpi-config/config.ini
@@ -6,7 +6,6 @@ allowAllConnections = true
 allowAllNat = true
 apDetect = false
 defaultOpenVlanId = 0
-killRunningProcess = false
 execAp = true
 execRadius = true
 execDhcp = true

dev-config.ini
@@ -11,7 +11,6 @@ generateSsid = true
 allocateVlans = true
 defaultOpenVlanId = 0
 quarantineVlanId = 10
-killRunningProcess = false
 execAp = false
 execRadius = true
 execDhcp = true

src/capture/header_middleware/mdns_decoder.h
@@ -26,6 +26,7 @@
 #ifndef MDNS_DECODER_H
 #define MDNS_DECODER_H
 
+#include "../../utils/net.h"
 #include "../../utils/os.h"
 #include "../../utils/utarray.h"
 

src/capture/header_middleware/packet_decoder.h
@@ -44,6 +44,7 @@
 #include "../../utils/utarray.h"
 #include "../../utils/allocs.h"
 #include "../../utils/os.h"
+#include "../../utils/net.h"
 
 #define MAX_QUESTION_LEN 255
 

commit fafba8d1dfe9075ea4b3ef9ac4c9102c6bb8d6f0

refactor(edgesec): remove unused `get_app_name()`
The `get_app_name()` function has not been used since
f7455135 (refactor, 2022-06-15)

Fixes: f74551353f899d8f31df1f8d9f16694b750a853f

src/edgesec.c
@@ -146,8 +146,6 @@ void process_app_options(int argc, char *argv[], uint8_t *verbosity,
   }
 }
 
-char *get_app_name(char *app_path) { return basename(app_path); }
-
 int main(int argc, char *argv[]) {
   bool daemon = false;
   uint8_t verbosity = 0;