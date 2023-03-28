60a9bfacde4b1138d59371af2bf6d2ea8e9378a3
Alois Klink <alois@nquiringminds.com>
2023-03-28 15:21:16 +0100

    refactor(dhcp): simplify get_config_dhcpinfo func
    Simplify the `get_config_dhcpinfo()` function.
    
    A lot of these checks can be removed if we just trust that
    `split_string_array()` works, or returns an error code if it fails.

--- src/dhcp/dhcp_config_utils.c
+++ src/dhcp/dhcp_config_utils.c
@@ -11,51 +11,37 @@ bool get_config_dhcpinfo(const char *info, config_dhcpinfo_t *el) {
   UT_array *info_arr;
   utarray_new(info_arr, &ut_str_icd);
 
-  if (split_string_array(info, ',', info_arr) < 0) {
+  ssize_t substrings = split_string_array(info, ',', info_arr);
+  if (substrings < 5) {
+    log_error("get_config_dhcpinfo: Expected 5 comma-separated substrings but "
+              "got %ld in string %s",
+              substrings, info);
     goto err;
   }
 
-  if (!utarray_len(info_arr)) {
+  char **vlanid_string = utarray_eltptr(info_arr, 0);
+  char **ip_addr_low = utarray_eltptr(info_arr, 1);
+  char **ip_addr_upp = utarray_eltptr(info_arr, 2);
+  char **subnet_mask = utarray_eltptr(info_arr, 3);
+  char **lease_time = utarray_eltptr(info_arr, 4);
+
+  errno = 0;
+  *el = (config_dhcpinfo_t){
+      .vlanid = (int)strtol(*vlanid_string, NULL, 10),
+  };
+  if (errno == EINVAL) {
+    log_errno("get_config_dhcpinfo: Failed to convert vlanid: %s to an integer",
+              *vlanid_string);
     goto err;
   }
 
-  char **p = NULL;
-  p = (char **)utarray_next(info_arr, p);
-  if (*p != NULL) {
-    errno = 0;
-    el->vlanid = (int)strtol(*p, NULL, 10);
-    if (errno == EINVAL)
-      goto err;
-  } else
-    goto err;
-
-  p = (char **)utarray_next(info_arr, p);
-  if (*p != NULL) {
-    os_strlcpy(el->ip_addr_low, *p, OS_INET_ADDRSTRLEN);
-  } else
-    goto err;
-
-  p = (char **)utarray_next(info_arr, p);
-  if (*p != NULL)
-    os_strlcpy(el->ip_addr_upp, *p, OS_INET_ADDRSTRLEN);
-  else
-    goto err;
-
-  p = (char **)utarray_next(info_arr, p);
-  if (*p != NULL)
-    os_strlcpy(el->subnet_mask, *p, OS_INET_ADDRSTRLEN);
-  else
-    goto err;
-
-  p = (char **)utarray_next(info_arr, p);
-  if (*p != NULL)
-    os_strlcpy(el->lease_time, *p, DHCP_LEASE_TIME_SIZE);
-  else
-    goto err;
+  os_strlcpy(el->ip_addr_low, *ip_addr_low, OS_INET_ADDRSTRLEN);
+  os_strlcpy(el->ip_addr_upp, *ip_addr_upp, OS_INET_ADDRSTRLEN);
+  os_strlcpy(el->subnet_mask, *subnet_mask, OS_INET_ADDRSTRLEN);
+  os_strlcpy(el->lease_time, *lease_time, DHCP_LEASE_TIME_SIZE);
 
   utarray_free(info_arr);
   return true;
-
 err:
   utarray_free(info_arr);
   return false;
